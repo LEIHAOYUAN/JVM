6d2be4b1d7e25a5ed5a8156d6ab99fb1a7322f20
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-03-14 17:53:07 +0800

    FEAT java valid demo
    Change-Id: I59ed96e0ca34beb54ab598af8722e7a548fb8f32

--- src/main/java/com/test/javax/Student.java
+++ src/main/java/com/test/javax/Student.java
@@ -1,10 +1,14 @@
 package com.test.javax;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 /**
  * @Description
@@ -13,7 +17,11 @@ import javax.validation.constraints.NotEmpty;
  */
 @Getter
 @Setter
-public class Student {
+@Builder
+public class Student implements Serializable {
+    private static final long serialVersionUID = -8223315658309712812L;
+    @NotNull(message = "年龄不能为空")
+    private Integer age;
     @NotBlank(message = "提货单号不能为空")
     private String pickNo;
     @NotEmpty(message = "姓名不能为空")
