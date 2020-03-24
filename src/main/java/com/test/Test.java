1a7219df1294970af3971bd7bcb82f21e88550ec
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-03-24 22:52:23 +0800

    FEAT test
    Change-Id: Ibc9de625816c40f217f88705c521222a9d36fc12

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -1,6 +1,12 @@
 package com.test;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Description
@@ -10,18 +16,25 @@ import org.springframework.util.StringUtils;
 public class Test {
 
     public static void main(String[] args) {
+        ParentPerson parentPerson = new ParentPerson();
+        parentPerson.setAge(100);
+        Person person = new Person();
+        System.out.println(person.getAge());
 
+    }
 
-        Long count = 0L;
-        if (count == null) {
-            System.out.println("===");
-            return;
+    private static void testList() {
+        List<Person> personList = Lists.newArrayList();
+        Person pp = new Person();
+        for (int i = 0; i < 5; i++) {
+            pp = new Person();
+            pp.setName("A" + i);
+            personList.add(pp);
         }
-        if (count <= 0) {
-            System.out.println("---");
-            return;
-        }
-        System.out.println("***");
+        System.out.println(personList.size());
+        System.out.println(JSON.toJSONString(personList));
+
+        System.out.println("--------------------------------------");
     }
 
     private static boolean validateRelationStockNo(String relationStockNo) {
@@ -33,4 +46,6 @@ public class Test {
         }
         return false;
     }
+
+
 }
