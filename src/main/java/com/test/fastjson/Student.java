df3edb14410f9601e4a26032c0c110099348ee11
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-10-12 12:09:37 +0800

    fastjson 测试
    Change-Id: I3338062c9c832eeb98489a923dcbdbdf060d4b3b

---
+++ src/main/java/com/test/fastjson/Student.java
@@ -0,0 +1,22 @@
+package com.test.fastjson;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Description
+ * @Author leihaoyuan
+ * @Date 2020/10/12 12:06
+ */
+@Getter
+@Setter
+public class Student {
+
+    @JSONField(serialize = false)
+    private Integer age;
+
+    @JSONField(name = "fullName")
+    private String name;
+
+}
---
+++ src/main/java/com/test/fastjson/Test.java
@@ -0,0 +1,24 @@
+package com.test.fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONField;
+
+/**
+ * @Description
+ * @Author leihaoyuan
+ * @Date 2020/10/12 12:03
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setAge(200);
+        student.setName("TESTXXXX");
+
+        System.out.println(JSON.toJSONString(student));
+
+    }
+
+
+}
