abb87ee25bc5da7890211cc6d101ca9e418f200e
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-11-16 13:02:43 +0800

    TEST calendar日期测试
    Change-Id: I14e3e1640ad8bd96a498efe4d207fb9deb1ddd9a

--- src/main/java/com/test/newdate/calendar/CalendarTest.java
+++ src/main/java/com/test/newdate/calendar/CalendarTest.java
@@ -16,7 +16,7 @@ public class CalendarTest {
         //获取年
         int year = c.get(Calendar.YEAR);
         //获取月份，0表示1月份
-        int month = c.get(Calendar.MONTH) + 1;
+        int month = c.get(Calendar.MONTH);
         //获取当前天数
         int day = c.get(Calendar.DAY_OF_MONTH);
         //获取本月最小天数
@@ -33,7 +33,7 @@ public class CalendarTest {
         SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
         String curDate = s.format(c.getTime());                                      //当前日期
-        System.out.println("当前时间：" + year + "-" + month + "-" + day + " " + time + ":" + min + ":" + sec);
+        System.out.println("当前时间：" + year + "年 " + month + "月 " + day + "天 " + time + "时 " + min + "分 " + sec +"秒");
         System.out.println("第一天和最后天：" + first +"," + last);
         System.out.println("当前日期：" + curDate);
     }
