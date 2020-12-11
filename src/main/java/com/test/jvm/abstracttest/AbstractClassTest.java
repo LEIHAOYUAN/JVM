c79965c4f93bfd8e634fc3f68add1c04d0d4fbd4
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-12-11 15:53:30 +0800

    抽象类实验
    Change-Id: I09ff7ba75d0e6c8c0dc04c8d73d52abd3214d26c

---
+++ src/main/java/com/test/jvm/abstracttest/AbstractClassTest.java
@@ -0,0 +1,31 @@
+package com.test.jvm.abstracttest;
+
+import java.lang.ref.WeakReference;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author leihaoyuan
+ * @Date 2020/12/11 13:17
+ */
+public abstract class AbstractClassTest {
+
+    private static final List<WeakReference<AbstractClassTest>> LISTENERS = Collections.synchronizedList(new LinkedList<>());
+
+    public AbstractClassTest(){
+        LISTENERS.add(new WeakReference<>(this));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LISTENERS.size());
+    }
+
+
+    private void test(){
+        System.out.println("test....");
+    }
+
+    abstract void aTtest();
+}
