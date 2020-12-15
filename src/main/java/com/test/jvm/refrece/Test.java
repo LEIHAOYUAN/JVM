5be1b53e9cd68334697dadc0e416bdab3960efb6
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-12-15 13:23:47 +0800

    测试引用类型赋值后地址
    Change-Id: I1b65a1384e4ccf28cd4fadbe907b19b90bfe45ee

---
+++ src/main/java/com/test/jvm/refrece/Test.java
@@ -0,0 +1,19 @@
+package com.test.jvm.refrece;
+
+import com.test.clone.Student;
+
+/**
+ * @Description 引用类型赋值测试
+ * @Author leihaoyuan
+ * @Date 2020/12/15 13:18
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Student s1 = new Student(90);
+        Student s2 = s1;
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
+    }
+
+}
