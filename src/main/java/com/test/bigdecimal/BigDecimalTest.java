5451a6a8ba6fa1599d6fcb00343c5f0004849457
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-12-16 19:59:43 +0800

    条件表达式测试
    Change-Id: I85fa6f0b378978c1ea1f3f178f8cf2fadfac814a

--- src/main/java/com/test/bigdecimal/BigDecimalTest.java
+++ src/main/java/com/test/bigdecimal/BigDecimalTest.java
@@ -10,18 +10,31 @@ import java.math.BigDecimal;
 public class BigDecimalTest {
 
     public static void main(String[] args) {
+
+    }
+
+    private void testCondition() {
+        int aa = 100;
+        if (aa > 10) {
+            System.out.println("第一次执行");
+        } else if (aa > 30) {
+            System.out.println("第二次执行");
+        }
+    }
+
+    private void testDetail() {
         System.out.println(BigDecimal.ZERO.compareTo(BigDecimal.TEN.negate()));
         System.out.println(BigDecimal.ZERO.negate().compareTo(BigDecimal.ZERO));
     }
 
-    private void testNullPoint(){
+    private void testNullPoint() {
         BigDecimal b = null;
         System.out.println(BigDecimal.ZERO.compareTo(b) == 0);
     }
 
-    private void test(){
-        BigDecimal decimal1=new BigDecimal("0");
-        BigDecimal decimal2=new BigDecimal(2);
+    private void test() {
+        BigDecimal decimal1 = new BigDecimal("0");
+        BigDecimal decimal2 = new BigDecimal(2);
         decimal1.add(decimal2);
         System.out.println(decimal1.add(decimal1));
         System.out.println(decimal1.add(decimal2));
@@ -31,7 +44,7 @@ public class BigDecimalTest {
     }
 
 
-    public static boolean testIsInteger(BigDecimal param){
+    public static boolean testIsInteger(BigDecimal param) {
         return param.remainder(BigDecimal.ONE).compareTo(BigDecimal.ZERO) == 0;
     }
 
