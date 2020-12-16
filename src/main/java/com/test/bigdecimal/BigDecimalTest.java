70f0382be19ee2251147c2ed02b7a1a9dc8bd28f
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-12-16 15:11:16 +0800

    Bigdecimal 空指针测试
    Change-Id: I0ebce2466372a653f6c9c830b17d1006a5280b4d

--- src/main/java/com/test/bigdecimal/BigDecimalTest.java
+++ src/main/java/com/test/bigdecimal/BigDecimalTest.java
@@ -10,6 +10,12 @@ import java.math.BigDecimal;
 public class BigDecimalTest {
 
     public static void main(String[] args) {
+        BigDecimal b = null;
+        System.out.println(BigDecimal.ZERO.compareTo(b) == 0);
+
+    }
+
+    private void test(){
         BigDecimal decimal1=new BigDecimal("0");
         BigDecimal decimal2=new BigDecimal(2);
         decimal1.add(decimal2);
@@ -18,7 +24,6 @@ public class BigDecimalTest {
 
         System.out.println("校验BigDecimal是否为整数");
         System.out.println(testIsInteger(BigDecimal.valueOf(1.0004)));
-
     }
 
 
