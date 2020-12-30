0f86a2354b1ebc0784cb68cee21a12ef8d4f2e3c
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-12-30 10:47:18 +0800

    迭代器移除后继续使用效果
    Change-Id: I1a1cd971dd3a0bfd3814a71116b6e5464f86c4f2

--- src/main/java/com/test/collection/iterator/ListTest.java
+++ src/main/java/com/test/collection/iterator/ListTest.java
@@ -15,37 +15,51 @@ import java.util.List;
 public class ListTest {
 
     public static void main(String[] args) {
+        test01();
+    }
+
+    private static void test(){
         Student s1 = new Student();
         s1.setName("AAA");
 
         Student s2 = new Student();
         s2.setName("AAA");
 
+        Student s3 = new Student();
+        s3.setName("BBB");
+
         List<Student> list = Lists.newArrayList();
         list.add(s1);
         list.add(s2);
+        list.add(s3);
         list.removeIf(item -> item.getName().equals("AAA"));
 
         System.out.println(JSON.toJSONString(list));
-
     }
 
-    private void test01(){
+    private static void test01(){
         Student s1 = new Student();
         s1.setName("AAA");
 
         Student s2 = new Student();
         s2.setName("AAA");
 
+        Student s3 = new Student();
+        s3.setName("BBB");
+
         List<Student> list = Lists.newArrayList();
         list.add(s1);
         list.add(s2);
+        list.add(s3);
         Iterator<Student> iterator = list.iterator();
         while (iterator.hasNext()){
             Student item = iterator.next();
             if(item.getName().equals("AAA")){
                 iterator.remove();
+                System.out.println("已经移除的："+item.toString());
+                continue;
             }
+            System.out.println("循环结束");
         }
 
         System.out.println(JSON.toJSONString(list));
