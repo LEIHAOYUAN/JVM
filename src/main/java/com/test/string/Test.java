ab586ed9aaeaf2d88f0decc284b11aed8f5242c9
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-01-21 16:40:35 +0800

    字符串拼接测试

--- src/main/java/com/test/string/Test.java
+++ src/main/java/com/test/string/Test.java
@@ -9,6 +9,7 @@ package com.test.string;
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("TEST".concat("     "));
         System.out.println("TEST".concat(null));
     }
 
