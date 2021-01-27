98af818847cb31c744bc138bb6ba16dde3b1df1e
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-01-27 15:40:35 +0800

    revert 测试01

--- src/main/java/com/test/json/RedisConfigModel.java
+++ src/main/java/com/test/json/RedisConfigModel.java
@@ -29,6 +29,10 @@ public class RedisConfigModel implements Serializable {
 
     private String name;
     private Integer age;
+    private Integer sex;
+
+    private String name;
+    private Integer age;
 
 
     public static void main(String[] args) {
