77361ca059c1b66047b0b9e0a64a164ff2838724
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-01-27 11:27:48 +0800

    revert push test

--- src/main/java/com/test/json/RedisConfigModel.java
+++ src/main/java/com/test/json/RedisConfigModel.java
@@ -38,8 +38,8 @@ public class RedisConfigModel implements Serializable {
         redisConfigModel.setJedisPoolMinIdel(0);
         redisConfigModel.setJedisPoolMaxWait(-1);
         System.out.println(JSON.toJSONString(redisConfigModel));
-        System.out.println("revert commit000000000");
-        System.out.println("revert commit");
+        System.out.println("revert commit0000");
+        System.out.println("revert commit00000000");
     }
 
 
