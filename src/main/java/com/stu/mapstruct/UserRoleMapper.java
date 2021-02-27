36262b9910bde342ab0d5e2c775f1c7a9d5e7a1e
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-02-27 14:25:46 +0800

    mapstruct转换关系映射测试

---
+++ src/main/java/com/stu/mapstruct/UserRoleMapper.java
@@ -0,0 +1,46 @@
+package com.stu.mapstruct;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface UserRoleMapper {
+
+    /**
+     * 获取该类自动生成的实现类的实例
+     */
+    UserRoleMapper INSTANCE = Mappers.getMapper(UserRoleMapper.class);
+
+    /**
+     * 使用单一对象
+     *
+     * @param user user
+     * @return UserRoleDto
+     */
+
+    @Mappings({
+            @Mapping(source = "id", target = "userId"),
+            @Mapping(source = "name", target = "userName"),
+            @Mapping(source = "addr", target = "userAddr"),
+            @Mapping(source = "role.name", target = "roleName"),
+    })
+    UserRoleDTO toUserRoleDto(User user);
+
+
+    /**
+     * 使用两个对象
+     *
+     * @param user user
+     * @param role role
+     * @return UserRoleDto
+     */
+    @Mappings({
+            @Mapping(source = "user.id", target = "userId"),
+            @Mapping(source = "user.name", target = "userName"),
+            @Mapping(source = "user.addr", target = "userAddr"),
+            @Mapping(source = "role.name", target = "roleName"),
+    })
+    UserRoleDTO toUserRoleDto(User user, Role role);
+}
