dc08d2d8fef738d5295218df522d7788df9bfc15
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-02-27 13:43:15 +0800

    mapstruct使用初探

---
+++ src/main/java/com/stu/mapstruct/Test.java
@@ -0,0 +1,33 @@
+package com.stu.mapstruct;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2021/2/27 13:24
+ * @Version 1.0
+ * @Description
+ */
+@Slf4j
+public class Test {
+
+    public static void main(String[] args) {
+        Person person = new Person(1L,"zhige","zhige.me@gmail.com",new Date(),new User(1));
+        PersonDTO personDTO = PersonConverter.INSTANCE.domain2dto(person);
+        log.info("单个对象转换：Person：{}，personDTO：{}",JSON.toJSONString(person), JSON.toJSONString(personDTO));
+        // 集合转换
+        List<Person> listPerson = new ArrayList<>();
+        listPerson.add(person);
+        List<PersonDTO> listPersonDTO = PersonConverter.INSTANCE.domain2dto(listPerson);
+        log.info("集合对象转换：listPerson：{}，listPersonDTO：{}",JSON.toJSONString(person), JSON.toJSONString(personDTO));
+    }
+
+}
---
+++ src/main/java/com/stu/mapstruct/User.java
@@ -0,0 +1,12 @@
+package com.stu.mapstruct;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    private Integer age;
+}
