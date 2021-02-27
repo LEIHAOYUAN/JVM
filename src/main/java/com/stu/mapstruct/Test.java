ce4b54565d10783ce3c5225dc98d330cd92fd567
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-02-27 14:10:36 +0800

    代码格式化

--- src/main/java/com/stu/mapstruct/Test.java
+++ src/main/java/com/stu/mapstruct/Test.java
@@ -2,13 +2,10 @@ package com.stu.mapstruct;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.time.DateFormatUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 /**
  * @Author leihaoyuan
@@ -20,14 +17,14 @@ import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 public class Test {
 
     public static void main(String[] args) {
-        Person person = new Person(1L,"zhige","zhige.me@gmail.com",new Date(),new User(1));
+        Person person = new Person(1L, "zhige", "zhige.me@gmail.com", new Date(), new User(1));
         PersonDTO personDTO = PersonConverter.INSTANCE.domain2dto(person);
-        log.info("单个对象转换：Person：{}，personDTO：{}",JSON.toJSONString(person), JSON.toJSONString(personDTO));
+        log.info("单个对象转换：Person：{}，personDTO：{}", JSON.toJSONString(person), JSON.toJSONString(personDTO));
         // 集合转换
         List<Person> listPerson = new ArrayList<>();
         listPerson.add(person);
         List<PersonDTO> listPersonDTO = PersonConverter.INSTANCE.domain2dto(listPerson);
-        log.info("集合对象转换：listPerson：{}，listPersonDTO：{}",JSON.toJSONString(person), JSON.toJSONString(personDTO));
+        log.info("集合对象转换：listPerson：{}，listPersonDTO：{}", JSON.toJSONString(person), JSON.toJSONString(personDTO));
     }
 
 }
