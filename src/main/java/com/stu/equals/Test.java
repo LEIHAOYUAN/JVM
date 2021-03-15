62a70d215559471cde97cbf662669fd48a4cd9cc
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-03-15 18:28:08 +0800

    JVM缓存-128到127之间的对象

---
+++ src/main/java/com/stu/equals/Test.java
@@ -0,0 +1,19 @@
+package com.stu.equals;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2021/3/15 18:25
+ * @Version 1.0
+ * @Description
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Integer a = 1000, b = 1000;
+        Integer c = 100, d = 100;
+        System.out.println(a == b);
+        System.out.println(c == d);
+
+    }
+
+}
