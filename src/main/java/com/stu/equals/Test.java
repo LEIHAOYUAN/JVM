7d64b9d0264232e580859896e904a8e828737b9d
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-03-16 16:22:41 +0800

    集合空值校验

--- src/main/java/com/stu/equals/Test.java
+++ src/main/java/com/stu/equals/Test.java
@@ -1,5 +1,10 @@
 package com.stu.equals;
 
+import com.google.common.collect.Lists;
+import com.stu.clone.Student;
+
+import java.util.List;
+
 /**
  * @Author leihaoyuan
  * @Date 2021/3/15 18:25
@@ -14,6 +19,10 @@ public class Test {
         System.out.println(a == b);
         System.out.println(c == d);
 
+        System.out.println("集合测试--------------------");
+        List<String> listParam = Lists.newArrayList();
+        listParam.add(null);
+        System.out.println(listParam.contains(new Student(12)));
     }
 
 }
