da10e64fbb323fad3910fc68fc335d865355a3d0
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-03-18 18:03:54 +0800

    SHA256加密测试

---
+++ src/main/java/com/util/security/Test.java
@@ -0,0 +1,21 @@
+package com.util.security;
+
+import cn.hutool.crypto.SecureUtil;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2021/3/18 18:01
+ * @Version 1.0
+ * @Description
+ */
+@Slf4j
+public class Test {
+
+    public static void main(String[] args) {
+        log.info("SHA-256：{}", SecureUtil.sha256("123456"));
+        log.info("SHA-1：{}", SecureUtil.sha1("123456"));
+
+    }
+
+}
