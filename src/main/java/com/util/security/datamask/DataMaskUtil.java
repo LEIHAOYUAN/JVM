4d6acab5f1ffc6d123a39a7ae418796d93cc4389
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-03-23 18:16:20 +0800

    Revert "TESt 11"

--- src/main/java/com/util/security/datamask/DataMaskUtil.java
+++ src/main/java/com/util/security/datamask/DataMaskUtil.java
@@ -13,7 +13,6 @@ public class DataMaskUtil {
 
     public static void main(String[] args) {
         System.out.println(hideMobile("15555522222"));
-        System.out.println("");
     }
 
     public static String hideMobile(String mobile) {
