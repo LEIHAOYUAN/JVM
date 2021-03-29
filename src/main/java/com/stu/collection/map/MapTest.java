ec32b3c118f52b4e69ddebd16101ad133b0e1d88
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-03-29 11:36:40 +0800

    map 过滤掉键为空的数据

--- src/main/java/com/stu/collection/map/MapTest.java
+++ src/main/java/com/stu/collection/map/MapTest.java
@@ -1,8 +1,15 @@
 package com.stu.collection.map;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @Author leihaoyuan
@@ -10,14 +17,21 @@ import java.util.HashMap;
  * @Version 1.0
  * @Description map操作
  */
+@Slf4j
 public class MapTest {
 
     public static void main(String[] args) {
-
+        test01();
     }
 
     private static void test01(){
-
+        Map<String,String> map = Maps.newHashMap();
+        map.put(null,"TEST%");
+        map.put("key1","test01");
+        map.put("key2","test02");
+        log.info("过滤前---------------------map数据：{}", JSON.toJSONString(map));
+        Map<String, String> filterMap = map.entrySet().stream().filter((i)-> StringUtils.isNotBlank(i.getKey())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        log.info("过滤后---------------------map数据：{}", JSON.toJSONString(filterMap));
     }
 
     private static void test02(){
