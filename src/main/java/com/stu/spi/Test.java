0daae8148115ac8dfa13c37f7e9de2bec9bd7115
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-03-30 09:27:33 +0800

    两种SPI使用方式

--- src/main/java/com/stu/spi/Test.java
+++ src/main/java/com/stu/spi/Test.java
@@ -14,14 +14,16 @@ import java.util.ServiceLoader;
 public class Test {
 
     public static void main(String[] args) {
+        // 方式1
         Iterator<SPIService> providers = Service.providers(SPIService.class);
-        ServiceLoader<SPIService> load = ServiceLoader.load(SPIService.class);
-
         while (providers.hasNext()) {
             SPIService ser = providers.next();
             ser.execute();
         }
+
         System.out.println("--------------------------------");
+        // 方式2
+        ServiceLoader<SPIService> load = ServiceLoader.load(SPIService.class);
         Iterator<SPIService> iterator = load.iterator();
         while (iterator.hasNext()) {
             SPIService ser = iterator.next();
