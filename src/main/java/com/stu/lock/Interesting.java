f666a9dbeacbada83e25c171b75a6040f516d783
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-04-20 13:33:40 +0800

    线程不安全测试样例

---
+++ src/main/java/com/stu/lock/Interesting.java
@@ -0,0 +1,48 @@
+package com.stu.lock;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2021/4/19 16:21
+ * @Version 1.0
+ * @Description
+ * 如果add、compare方法没有加锁，并且由于a方法和b方法中的变量操作都不是原子性操作，
+ * 所以会出现和预期结果不一致！！！
+ * 解决方案：
+ * 需要在add方法和compare方法上都加上锁
+ */
+@Slf4j
+public class Interesting {
+
+    public static void main(String[] args) {
+        Interesting interesting = new Interesting();
+        new Thread(() -> interesting.add()).start();
+        new Thread(() -> interesting.compare()).start();
+    }
+
+    volatile int a = 1;
+    volatile int b = 1;
+
+    public synchronized void add() {
+        log.info("add start");
+        for (int i = 0; i < 10000; i++) {
+            a++;
+            b++;
+        }
+        log.info("add done");
+    }
+
+    public synchronized void compare() {
+        log.info("compare start");
+        for (int i = 0; i < 10000; i++) {
+            //a始终等于b吗？
+            if (a < b) {
+                log.info("a小于b=============a:{},b:{},{}", a, b, a > b);
+            }
+        }
+        log.info("compare done");
+    }
+
+
+}
