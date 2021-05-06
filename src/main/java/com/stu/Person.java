2fd4614e0871f2b89e40e3e2c70d5e64c5592722
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-05-06 14:43:26 +0800

    测试对象指针

--- src/main/java/com/stu/Person.java
+++ src/main/java/com/stu/Person.java
@@ -1,10 +1,40 @@
 package com.stu;
 
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
-@Setter
-@Getter
+@Slf4j
+@Data
 public class Person extends  ParentPerson{
     private String name;
+
+    public static void main(String[] args) {
+        samePoint1();
+    }
+
+    private static void samePoint1(){
+        Person p1 = new Person();
+        p1.setName("张三");
+
+        Person p2 = new Person();
+        p2 = p1;
+        p2.setName("李四");
+
+        log.info("p1对象：{}", JSON.toJSONString(p1));
+        log.info("p2对象：{}", JSON.toJSONString(p2));
+    }
+
+    private static void samePoint(){
+        Person p1 = new Person();
+        p1.setName("张三");
+
+        Person p2 = p1;
+        p2.setName("李四");
+
+        log.info("p1对象：{}", JSON.toJSONString(p1));
+        log.info("p2对象：{}", JSON.toJSONString(p2));
+    }
 }
