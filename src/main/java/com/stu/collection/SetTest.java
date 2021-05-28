47407da178ddd7c5773450e8944430f528690ec8
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-05-28 10:45:46 +0800

    array to set

--- src/main/java/com/stu/collection/SetTest.java
+++ src/main/java/com/stu/collection/SetTest.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -16,12 +18,23 @@ import java.util.Set;
 public class SetTest {
 
     public static void main(String[] args) {
+        arrayToSet();
+    }
+
+    private static void test(){
         Set<Long> unitCodeSet = Sets.newHashSet();
         unitCodeSet.add(null);
         unitCodeSet.add(null);
         unitCodeSet.add(90L);
         unitCodeSet.add(90L);
         log.info("set结果：{}", JSON.toJSONString(unitCodeSet));
+    }
+
+    private static void arrayToSet(){
+        String[] strArray= new String[]{"Tom", "Bob", "Jane","Tom0", "Bob", "Jane"};
+        HashSet<String> strings = Sets.newHashSet(Arrays.asList(strArray));
+        strings.add("AAA");
+        log.info("结果：{}",JSON.toJSONString(strings));
     }
 
 
