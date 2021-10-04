35218e54a4484e947732db65b64b3893f77f0f1b
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-10-04 16:52:52 +0800

    字符串切分结果测试

--- stu/src/main/java/com/lei/stu/base/StringTest.java
+++ stu/src/main/java/com/lei/stu/base/StringTest.java
@@ -12,15 +12,24 @@ import lombok.extern.slf4j.Slf4j;
 public class StringTest {
 
     public static void main(String[] args) {
-        testSplit();
+        testContins();
     }
 
 
-    public static void testSplit(){
+    public static void testContins() {
+        String param = "www.baidu.com,www.jd.com,www.tx.com,,";
+        String[] split = param.split(",");
+        for (String s : split) {
+            log.info("字符串切分结果：{}", s);
+        }
+    }
+
+
+    public static void testSplit() {
         String param = "http://www.baidu.comhttp://www.alibaba.com888,558";
         String[] urls = param.split(",");
         for (String url : urls) {
-            log.info("拆分URL：{}",url);
+            log.info("拆分URL：{}", url);
         }
     }
 
