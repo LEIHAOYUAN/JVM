c4156c0ff4b493f43c1f1c9f4717ed69f3fdf822
haoyuan.lei <haoyuan.lei@joymo.tech>
2021-10-13 18:20:59 +0800

    列表复制工具类

--- utils/src/main/java/com/base/utils/copy/CopyUtil.java
+++ utils/src/main/java/com/base/utils/copy/CopyUtil.java
@@ -1,6 +1,10 @@
 package com.base.utils.copy;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author leihaoyuan
@@ -29,8 +33,28 @@ public class CopyUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return obj;
+    }
+
+    /**
+     * 列表复制
+     *
+     * @param source
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> copyList(List source, Class<T> clazz) {
+        List<T> target = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(source)) {
+            if (!CollectionUtils.isEmpty(source)) {
+                for (Object c : source) {
+                    T obj = copy(c, clazz);
+                    target.add(obj);
+                }
+            }
+        }
+        return target;
     }
 
 }
