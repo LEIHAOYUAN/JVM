18a617a7fa415e415688841084b9b112ff8930a4
haoyuan.lei <haoyuan.lei@abite.com>
2021-10-26 18:08:46 +0800

    事务传播行为以及捕获异常处理测试

---
+++ stu/src/test/java/service/transaction/Test.java
@@ -0,0 +1,33 @@
+package service.transaction;
+
+import com.lei.stu.transaction.rollbackonly.AService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2021/10/26 18:02
+ * @Version 1.0
+ * @Description
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+public class Test {
+
+    @Resource
+    private AService aService;
+
+    @org.junit.Test
+    public void test() {
+        aService.methodA();
+    }
+
+
+}
