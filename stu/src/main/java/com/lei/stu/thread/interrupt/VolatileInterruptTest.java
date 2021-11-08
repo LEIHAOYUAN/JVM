d4cafc8f45cdff2775ca95f693fe9398717ded21
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-08 11:22:25 +0800

    FEAT 使用volatile控制线程

--- stu/src/main/java/com/lei/stu/thread/interrupt/VolatileInterruptTest.java
+++ stu/src/main/java/com/lei/stu/thread/interrupt/VolatileInterruptTest.java
@@ -4,12 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Author leihaoyuan
- * @Date 2021/11/6 14:19
+ * @Date 2021/11/8 11:00
  * @Version 1.0
- * @Description 使用interrupt控制线程
+ * @Description 使用volatile控制线程
  */
 @Slf4j
-public class InterruptTest {
+public class VolatileInterruptTest {
+
+    /**
+     * 控制线程变量
+     */
+    private static volatile boolean NOTIFY_FLAG = true;
 
     public static void main(String[] args) throws InterruptedException {
         log.info("main方法执行start.......");
@@ -17,7 +22,8 @@ public class InterruptTest {
         thread.start();
         // 主线程休眠
         Thread.sleep(1000);
-        thread.interrupt();
+        // 更新变量
+        NOTIFY_FLAG = false;
         // 等待thread子线程结束后继续执行main线程
         thread.join();
         log.info("main方法执行end.........");
@@ -27,7 +33,7 @@ public class InterruptTest {
     private static class InterruptThread extends Thread {
         @Override
         public void run() {
-            while (!Thread.currentThread().isInterrupted()) {
+            while (NOTIFY_FLAG) {
                 log.info("线程正在执行。。。。。。。。。");
             }
             log.info("被中断!!!!!!");
