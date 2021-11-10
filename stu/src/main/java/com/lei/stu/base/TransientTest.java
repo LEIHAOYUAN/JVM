a3fda5d726dd7c76a8d524b8da286f30af92ebf7
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-10 11:16:10 +0800

    测试transient不影响fastjson序列化

---
+++ stu/src/main/java/com/lei/stu/base/TransientTest.java
@@ -0,0 +1,35 @@
+package com.lei.stu.base;
+
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.Transient;
+import java.io.Serializable;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2021/11/10 11:07
+ * @Version 1.0
+ * @Description Transient测试
+ */
+@Slf4j
+@Data
+public class TransientTest implements Serializable {
+
+    private static final long serialVersionUID = -4720187174176955837L;
+
+    private String name;
+
+    @Transient
+    private String remark;
+
+    public static void main(String[] args) {
+        TransientTest param = new TransientTest();
+        param.setName("AAA");
+        param.setRemark("备注信息");
+        log.info("transient序列化测试：{}", JSON.toJSONString(param));
+    }
+
+
+}
