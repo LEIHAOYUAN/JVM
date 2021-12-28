06e64e7c8293c25e6af3c330a432d4683ac198b3
haoyuan.lei <haoyuan.lei@abite.com>
2021-12-28 13:47:43 +0800

    平滑权重轮询算法

---
+++ utils/src/main/java/com/base/algorithm/loadbalance/SmoothWeightRoundRobin.java
@@ -0,0 +1,71 @@
+package com.base.algorithm.loadbalance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2021/12/28 11:20
+ * @Version 1.0
+ * @Description 平滑的带有权重的轮询算法
+ * https://www.dazhuanlan.com/balitaimonk/topics/1279481
+ */
+public class SmoothWeightRoundRobin {
+
+    public static void main(String[] args) {
+        List<Server> servers = new ArrayList<>();
+        servers.add(new Server("AAA", 1));
+        servers.add(new Server("BBB", 2));
+        servers.add(new Server("CCC", 3));
+        servers.add(new Server("DDD", 4));
+        // 平滑权重轮询算法
+        SmoothWeightRoundRobin swrr = new SmoothWeightRoundRobin(servers);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(swrr.next());
+        }
+    }
+
+
+    public static class Server {
+        private String address;
+        private int weight;
+        private int currentWeight;
+
+        public Server(String address, int weight) {
+            this.address = address;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return "Server{address='" + address + ", weight=" + weight + ", currentWeight=" + currentWeight + '}';
+        }
+    }
+
+
+    private List<Server> servers;
+    private int totalWeight;
+
+    public SmoothWeightRoundRobin(List<Server> servers) {
+        this.servers = servers;
+        totalWeight = servers.stream().map(server -> server.weight).reduce((a, b) -> a + b).get();
+    }
+
+    public Server next() {
+        Server best = null;
+        for (Server server : servers) {
+            // step1:更新当前权重
+            server.currentWeight += server.weight;
+            // step2：找到最大的权重
+            if (best == null || best.currentWeight < server.currentWeight) {
+                best = server;
+            }
+        }
+        // step3：最大的权重减去总权重
+        if (best != null) {
+            best.currentWeight -= totalWeight;
+        }
+        return best;
+    }
+
+}
