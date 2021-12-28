e06df55d7b2dfd59d7c7f7c970e09515be33d928
haoyuan.lei <haoyuan.lei@abite.com>
2021-12-28 15:50:23 +0800

    测试Set中基本类型顺序

--- stu/src/main/java/com/lei/stu/collection/SetTest.java
+++ stu/src/main/java/com/lei/stu/collection/SetTest.java
@@ -18,30 +18,34 @@ import java.util.Set;
 public class SetTest {
 
     public static void main(String[] args) {
-        convertTypeArrayToSet();
+        testSequence();
     }
 
-    private static void test(){
+    private static void testSequence() {
         Set<Long> unitCodeSet = Sets.newHashSet();
+        unitCodeSet.add(5L);
         unitCodeSet.add(null);
         unitCodeSet.add(null);
         unitCodeSet.add(90L);
-        unitCodeSet.add(90L);
+        unitCodeSet.add(60L);
+        unitCodeSet.add(23L);
+        unitCodeSet.add(0L);
+        unitCodeSet.add(1L);
         log.info("set结果：{}", JSON.toJSONString(unitCodeSet));
     }
 
-    private static void arrayToSet(){
-        String[] strArray= new String[]{"1", "111", "2","21", "22", "222"};
+    private static void arrayToSet() {
+        String[] strArray = new String[]{"1", "111", "2", "21", "22", "222"};
         HashSet<String> strings = Sets.newHashSet(Arrays.asList(strArray));
         strings.add("AAA");
-        log.info("结果：{}",JSON.toJSONString(strings));
+        log.info("结果：{}", JSON.toJSONString(strings));
     }
 
-    private static void convertTypeArrayToSet(){
-        String[] strArray= new String[]{"1", "111", "2","21", "22", "222"};
+    private static void convertTypeArrayToSet() {
+        String[] strArray = new String[]{"1", "111", "2", "21", "22", "222"};
         HashSet<String> strings = Sets.newHashSet(Arrays.asList(strArray));
         strings.add("AAA");
-        log.info("结果：{}",JSON.toJSONString(strings));
+        log.info("结果：{}", JSON.toJSONString(strings));
     }
 
 
