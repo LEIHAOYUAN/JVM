46ebea9e906f6db30e3f90c76c2ed354d33155bb
haoyuan.lei <haoyuan.lei@abite.com>
2021-12-28 15:35:25 +0800

    随机权重轮询算法实现

--- utils/src/main/java/com/base/algorithm/loadbalance/RoundRobinByRandomWeight.java
+++ utils/src/main/java/com/base/algorithm/loadbalance/RoundRobinByRandomWeight.java
@@ -1,5 +1,6 @@
 package com.base.algorithm.loadbalance;
 
+import com.alibaba.fastjson.JSON;
 import com.base.algorithm.loadbalance.model.Node;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,8 @@ import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -20,10 +23,10 @@ public class RoundRobinByRandomWeight {
 
     public static void main(String[] args) {
         List<Node> param = Lists.newArrayList();
-        param.add(new Node("AAA", 4));
-        param.add(new Node("BBB", 2));
+        param.add(new Node("AAA", 6));
+        param.add(new Node("BBB", 3));
         param.add(new Node("CCC", 1));
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 1000; i++) {
             Node node = choose(param);
             log.info("第{}次-轮询结果：{}", i, null == node ? null : node.getAddress());
         }
@@ -47,45 +50,39 @@ public class RoundRobinByRandomWeight {
             int totalWeight = param.stream().mapToInt(Node::getWeight).sum();
             // 按照权重分组，防止有相同权重数据
             Map<Integer, List<Node>> weightCollect = param.stream().collect(Collectors.groupingBy(Node::getWeight, Collectors.toList()));
-
-
+            // 获取权重组
+            List<Node> matchNodes = weightCollect.get(selectWeight(totalWeight, weightCollect.keySet()));
+            if (null == matchNodes) {
+                return null;
+            } else if (matchNodes.size() == 1) {
+                return matchNodes.stream().findFirst().orElse(null);
+            } else {
+                return RoundRobin.choose(matchNodes);
+            }
         }
-
-
         return null;
     }
 
+    /**
+     * 使用随机算法筛选权重值
+     *
+     * @param totalWeight 总权重
+     * @param weights 权重集合
+     * @return
+     */
+    private static int selectWeight(int totalWeight, Set<Integer> weights) {
+        int offset = new Random().nextInt(totalWeight);
+        for (Integer weight : weights) {
+            // 用此随机数循环减去服务提供者的权重直到改值<0,则此服务提供者为目标服务提供者
+            offset -= weight;
+            if (offset < 0) {
+                return weight;
+            }
+        }
+        log.warn("未匹配到权重，offset：{}，权重集合：{}", offset, JSON.toJSONString(weights));
+        return -1;
+    }
 
-    // protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
-    //     int length = invokers.size(); // Number of invokers
-    //     boolean sameWeight = true; // Every invoker has the same weight?
-    //     int firstWeight = getWeight(invokers.get(0), invocation);
-    //     int totalWeight = firstWeight; // The sum of weights
-    //     //判断是否所有服务提供者的权重都相同
-    //     for (int i = 1; i < length; i++) {
-    //         int weight = getWeight(invokers.get(i), invocation);
-    //         totalWeight += weight; // Sum
-    //         if (sameWeight && weight != firstWeight) {
-    //             sameWeight = false;
-    //         }
-    //     }
-    //     //所有服务提供者的权重不都相同，且权重之和>0，则随机生成一个0-totalWeight的随机数，
-    //     // 然后用此随机数循环减去服务提供者的权重直到改值<0,则此服务提供者为目标服务提供者
-    //     if (totalWeight > 0 && !sameWeight) {
-    //         // If (not every invoker has the same weight & at least one invoker's weight>0), select randomly based on totalWeight.
-    //         int offset = ThreadLocalRandom.current().nextInt(totalWeight);
-    //         // Return a invoker based on the random value.
-    //         for (int i = 0; i < length; i++) {
-    //             offset -= getWeight(invokers.get(i), invocation);
-    //             if (offset < 0) {
-    //                 return invokers.get(i);
-    //             }
-    //         }
-    //     }
-    //     // If all invokers have the same weight value or totalWeight=0, return evenly.
-    //     //所有服务提供者的权重都相同，则随机选取一个服务提供者,则此服务提供者为目标服务提供者
-    //     return invokers.get(ThreadLocalRandom.current().nextInt(length));
-    // }
 
 }
 
