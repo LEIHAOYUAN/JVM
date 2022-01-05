6751f5efaa94d4ad38d10e1294cae46aff02d1e0
haoyuan.lei <haoyuan.lei@abite.com>
2022-01-05 17:34:58 +0800

    测试线程池以及定时任务线程池

--- utils/src/main/java/com/base/utils/thread/ThreadPoolUtil.java
+++ utils/src/main/java/com/base/utils/thread/ThreadPoolUtil.java
@@ -3,19 +3,22 @@ package com.base.utils.thread;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * 该线程池工具类
  *
- * @author: 颜伟晗
+ * @author:
  * @time: 2021/05/24
  */
 @Slf4j
 public class ThreadPoolUtil {
     private static final ThreadPoolExecutor POOL = create(8, 2000);
+
+    /**
+     * 定时任务线程池
+     */
+    private static final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(2);
 
     /**
      * @param size
@@ -39,5 +42,18 @@ public class ThreadPoolUtil {
 
     public static void execute(Runnable runnable) {
         POOL.execute(runnable);
+    }
+
+    public static void main(String[] args) {
+        // 测试定时任务
+        // scheduledExecutor.schedule(() -> log.info("延迟任务执行完毕......"),10,TimeUnit.SECONDS);
+        // scheduledExecutor.shutdown();
+        // 测试线程池
+        execute(new Runnable() {
+            @Override
+            public void run() {
+                log.info("异步任务执行完毕.........");
+            }
+        });
     }
 }
