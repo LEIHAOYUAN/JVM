d112c5b2a949b2c5cca679200ce631d69a3ad4d4
haoyuan.lei <haoyuan.lei@abite.com>
2022-01-21 14:49:26 +0800

    java8元空间-方法区内存溢出实验

---
+++ stu/src/main/java/com/lei/jvm/JavaMethodAreaOOM.java
@@ -0,0 +1,48 @@
+package com.lei.jvm;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2022/1/21 14:43
+ * @Version 1.0
+ * @Description 方法区内存溢出验证
+ * 在JDK 8以后，永久代便完全退出了历史舞台，元空间作为其替代者登场。在默认设置下，前面
+ * 列举的那些正常的动态创建新类型的测试用例已经很难再迫使虚拟机产生方法区的溢出异常了。
+ * --------------------------------------------------------------------------------------
+ * ·-XX：MaxMetaspaceSize：设置元空间最大值，默认是-1，即不限制，或者说只受限于本地内存大小。
+ * ·-XX：MetaspaceSize：指定元空间的初始空间大小，以字节为单位，达到该值就会触发垃圾收集进行类型卸载，同时收集器会对该值进行调整：如果释放了大量的空间，就适当降低该值；如果释放了很少的空间，那么在不超过-XX：MaxMetaspaceSize（如果设置了的话）的情况下，适当提高该值。
+ * ·-XX：MinMetaspaceFreeRatio：作用是在垃圾收集之后控制最小的元空间剩余容量的百分比，可减少因为元空间不足导致的垃圾收集的频率。类似的还有-XX：Max-MetaspaceFreeRatio，用于控制最大的元空间剩余容量的百分比。
+ */
+public class JavaMethodAreaOOM {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 借助CGLib使得方法区出现内存溢出异常
+     */
+    private static void oomWithCGlib() {
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invokeSuper(obj, args);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class OOMObject {
+    }
+
+
+}
