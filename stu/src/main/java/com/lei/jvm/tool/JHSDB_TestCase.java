e30b9da709f51c8bf5c3811a8147661fe9453ad9
haoyuan.lei <haoyuan.lei@abite.com>
2022-01-26 11:18:52 +0800

    添加代码注释

--- stu/src/main/java/com/lei/jvm/tool/JHSDB_TestCase.java
+++ stu/src/main/java/com/lei/jvm/tool/JHSDB_TestCase.java
@@ -2,6 +2,11 @@ package com.lei.jvm.tool;
 
 /**
  * staticObj、instanceObj、localObj存放在哪里？
+ *
+ * 背景：
+ * 从《Java虚拟机规范》所定义的概念模型来看，所有Class相关的信息都应该存放在方法区之中，
+ * 但方法区该如何实现，《Java虚拟机规范》并未做出规定，这就成了一件允许不同虚拟机自己灵活把握的事情。
+ * JDK 7及其以后版本的HotSpot虚拟机选择把静态变量与类型在Java语言一端的映射Class对象存放在一起，存储于Java堆之中
  */
 public class JHSDB_TestCase {
     static class Test {
