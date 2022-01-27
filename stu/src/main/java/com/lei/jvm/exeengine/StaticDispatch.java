c474f57dcf185084bffb67b1320463f9288ea86c
haoyuan.lei <haoyuan.lei@abite.com>
2022-01-27 17:45:47 +0800

    静态分派代码演示

---
+++ stu/src/main/java/com/lei/jvm/exeengine/StaticDispatch.java
@@ -0,0 +1,39 @@
+package com.lei.jvm.exeengine;
+
+/**
+ * @Author leihaoyuan
+ * @Date 2022/1/27 17:42
+ * @Version 1.0
+ * @Description 方法静态分派演示
+ */
+public class StaticDispatch {
+
+    static abstract class Human {
+    }
+
+    static class Man extends Human {
+    }
+
+    static class Woman extends Human {
+    }
+
+    public void sayHello(Human guy) {
+        System.out.println("hello,guy!");
+    }
+
+    public void sayHello(Man guy) {
+        System.out.println("hello,gentleman!");
+    }
+
+    public void sayHello(Woman guy) {
+        System.out.println("hello,lady!");
+    }
+
+    public static void main(String[] args) {
+        Human man = new Man();
+        Human woman = new Woman();
+        StaticDispatch sr = new StaticDispatch();
+        sr.sayHello(man);
+        sr.sayHello(woman);
+    }
+}
---
+++ stu/src/main/java/com/lei/jvm/exeengine/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Author leihaoyuan
+ * @Date 2022/1/27 17:42
+ * @Version 1.0
+ * @Description 虚拟机字节码执行引擎
+ */
+package com.lei.jvm.exeengine;
