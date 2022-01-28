bd7626fb016672963e4cc9f90f01673a50e55c27
haoyuan.lei <haoyuan.lei@abite.com>
2022-01-28 16:26:39 +0800

    自动拆箱的陷阱

--- stu/src/main/java/com/lei/stu/base/IntegerTest.java
+++ stu/src/main/java/com/lei/stu/base/IntegerTest.java
@@ -12,7 +12,27 @@ import lombok.extern.slf4j.Slf4j;
 public class IntegerTest {
 
     public static void main(String[] args) {
-        testEquals();
+
+    }
+
+    /**
+     * 关于自动拆箱的错误使用案例
+     * 包装类的“==”运算在不遇到算术运算的情况下不会自动拆箱，以及它们equals()方法不处理数据转型的关系
+     */
+    private static void errorCase() {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
     }
 
     private static void testEquals() {
