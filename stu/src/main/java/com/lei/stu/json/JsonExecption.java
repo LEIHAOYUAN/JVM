36a95896107cedd3fab9c2f79b62492a8c00943a
leihaoyuan <haoyuan.lei@exe.com>
2022-07-27 15:25:26 +0800

    测试JSON序列化异常

---
+++ stu/src/main/java/com/lei/stu/json/JsonExecption.java
@@ -0,0 +1,22 @@
+package com.lei.stu.json;
+
+import com.alibaba.fastjson.JSON;
+import com.lei.stu.clone.Student;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *  职能描述：测试JSON转换异常
+ *  @author leihaoyuan
+ *  @version 2022/7/27 10:31
+ */
+@Slf4j
+public class JsonExecption {
+
+
+    public static void main(String[] args) {
+        String json = null;
+        Student student = JSON.parseObject(json, Student.class);
+        log.info("转换结果：{}", JSON.toJSONString(student));
+    }
+
+}
