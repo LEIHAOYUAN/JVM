621d7f32a739ce9f3add6c7f97904f4354b569c4
leihaoyuan <haoyuan.lei@exe.com>
2022-09-26 12:30:03 +0800

    测试SPEL表达式

--- stu/src/main/java/com/lei/spel/SpELTest.java
+++ stu/src/main/java/com/lei/spel/SpELTest.java
@@ -7,7 +7,7 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 /**
- *  职能描述：
+ *  职能描述：测试SPEL解析表达式
  *  @author leihaoyuan
  *  @version 2022/8/16 11:09
  */
@@ -16,18 +16,31 @@ public class SpELTest {
 
 
     public static void main(String[] args) {
-        ExpressionParser parser =  new SpelExpressionParser();
-        String template = "#test";
-        Expression expression = parser.parseExpression(template);
-
-        // expression context
-        StandardEvaluationContext context = new StandardEvaluationContext();
-        context.setVariable("test","值");
-        log.info("解析到属性：{}",expression.getValue(context,Object.class));
-
-
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("getInput()");
+        StandardEvaluationContext ctx = new StandardEvaluationContext();
+        User user = new User("张三");
+        // 设置需要执行方法的类
+        ctx.setRootObject(user);
+        String value = expression.getValue(ctx, String.class);
+        log.info("获取结果：{}", value);
     }
 
+    public static class User {
+        private String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String getInput() {
+            return "我叫：" + this.name;
+        }
+    }
 
 
 }
