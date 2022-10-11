226d190ad055c72059826ebd8e8ee99fd51a83b8
haoyuan.lei <646914508@qq.com>
2022-10-11 10:52:54 +0800

    测试字节数组-对象相互转换

--- stu/src/main/java/com/lei/jvm/stu/serialization/json/FastJsonTest.java
+++ stu/src/main/java/com/lei/jvm/stu/serialization/json/FastJsonTest.java
@@ -5,6 +5,7 @@ import com.lei.jvm.stu.serialization.SerializationInfo;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 
 /**
  *  职能描述：
@@ -16,16 +17,33 @@ import java.math.BigDecimal;
 public class FastJsonTest {
 
     public static void main(String[] args) {
-        SerializationInfo info = new SerializationInfo(100L, "hessian", BigDecimal.TEN);
-        info.setName("AAA");
+        testByte();
+    }
+
+    private static void testParseObject() {
+        SerializationInfo info = buildObject();
         log.info("json格式：{}", JSON.toJSONString(info));
 
         String demoJson = "{\"name\":\"AAA\"}";
         SerializationInfo o = JSON.parseObject(demoJson, SerializationInfo.class);
-        log.info("JSON反序列化:{}",o.getId());
+        log.info("JSON反序列化:{}", o.getId());
     }
 
+    private static void testByte() {
+        SerializationInfo info = buildObject();
+        byte[] bytes = JSON.toJSONString(info).getBytes(StandardCharsets.UTF_8);
+        String json = new String(bytes, StandardCharsets.UTF_8);
+        log.info("字节数据转换JSON={}", json);
+        SerializationInfo serializationInfo = JSON.parseObject(json, SerializationInfo.class);
+        log.info("转换对象={}", serializationInfo.getName());
 
+    }
+
+    private static SerializationInfo buildObject() {
+        SerializationInfo info = new SerializationInfo(100L, "hessian", BigDecimal.TEN);
+        info.setName("AAA");
+        return info;
+    }
 
 
 }
