213cadf6413147936740eb0ac18cedf1e2b8f11c
haoyuan.lei <646914508@qq.com>
2022-10-17 13:13:44 +0800

    验证caffeine覆盖更新

--- stu/src/main/java/com/lei/jvm/stu/caffeine/CaffeineTest.java
+++ stu/src/main/java/com/lei/jvm/stu/caffeine/CaffeineTest.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
  *  @version 2022/9/15 11:12
  */
 @Slf4j
-public class CacheTest {
+public class CaffeineTest {
 
     public static void main(String[] args) {
         String item = "AAAAAAAA";
@@ -19,10 +19,10 @@ public class CacheTest {
         for (int i = 1; i <= 5000; i++) {
             cache.put(i, item + i);
         }
-        log.info("查询缓存结果：{}", JSON.toJSONString(cache.getIfPresent(5000)));
-        cache.invalidate(5000-10);
-        log.info("查询缓存结果：{}", JSON.toJSONString(cache.getIfPresent(5000-10)));
-        log.info("查询缓存结果：{}", JSON.toJSONString(cache.getIfPresent(5000-11)));
+        cache.put(1,item+1);
+        cache.put(1,item+2);
+        cache.put(1,item+3);
+        log.info("查询缓存结果：{}", JSON.toJSONString(cache.getIfPresent(1)));
     }
 
 
@@ -30,7 +30,7 @@ public class CacheTest {
         return Caffeine
                 .newBuilder()
                 //设置缓存的 Entries 个数最多不超过1000个
-                .maximumSize(5000)
+                .maximumSize(1)
                 .build();
     }
 
