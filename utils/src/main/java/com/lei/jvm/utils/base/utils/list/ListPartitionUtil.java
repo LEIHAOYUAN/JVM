694b9f9f1e8c96048d5270d6b0512660a8ee74e2
haoyuan.lei <646914508@qq.com>
2022-11-03 10:15:18 +0800

    集合切分工具类

---
+++ utils/src/main/java/com/lei/jvm/utils/base/utils/list/ListPartitionUtil.java
@@ -0,0 +1,105 @@
+package com.lei.jvm.utils.base.utils.list;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.ListUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *  职能描述：集合切分工具类
+ *  @author leihaoyuan
+ *  @version 2022/11/3 10:08
+ *  https://www.cnblogs.com/zhanh247/p/11842748.html
+ */
+@Slf4j
+public class ListPartitionUtil {
+
+    /**
+     * 存java处理
+     * @param list
+     * @param size
+     * @return
+     * @param <T>
+     */
+    public static <T> List<List<T>> partitioByBase(final List<T> list, final int size) {
+        List<List<T>> result = new ArrayList<List<T>>();
+        final int listSize = list.size();
+        for (int i = 0; i < listSize; i += size) {
+            List<T> subList = null;
+            if (i + size > listSize) {
+                subList = list.subList(i, i + listSize - i);
+            } else {
+                subList = list.subList(i, i + size);
+            }
+            result.add(subList);
+        }
+        return result;
+    }
+
+
+    /**
+     * 使用guava工具类
+     * @param param
+     * @param size
+     * @return
+     * @param <T>
+     */
+    public static <T> List<List<T>> partitionByGuava(List<T> param, int size) {
+        if (CollectionUtils.isEmpty(param)) {
+            return Lists.newArrayList();
+        }
+        return Lists.partition(param, size);
+    }
+
+    /**
+     * 使用apache工具类
+     * @param param
+     * @param size
+     * @return
+     * @param <T>
+     */
+    public static <T> List<List<T>> partitionByApache(List<T> param, int size) {
+        if (CollectionUtils.isEmpty(param)) {
+            return Lists.newArrayList();
+        }
+        return ListUtils.partition(param, size);
+    }
+
+    /**
+     * 使用流遍历操作
+     * @param list
+     * @param size
+     * @return
+     * @param <T>
+     */
+    private static <T> List<List<T>> partitionBySimpleStream(final List<T> list, final int size) {
+        Integer limit = (list.size() + size - 1) / size;
+        List<List<T>> mglist = new ArrayList<List<T>>();
+        Stream.iterate(0, n -> n + 1).limit(limit).forEach(i -> {
+            mglist.add(list.stream().skip(i * size).limit(size).collect(Collectors.toList()));
+        });
+        return mglist;
+    }
+
+    /**
+     * 使用流并行处理
+     * @param list
+     * @param size
+     * @return
+     * @param <T>
+     */
+    private static <T> List<List<T>> partitionByParallelStream(final List<T> list, final int size) {
+        Integer limit = (list.size() + size - 1) / size;
+        List<List<T>> splitList = Stream.iterate(0, n -> n + 1).limit(limit).parallel()
+                .map(a -> list.stream().skip(a * size).limit(size).parallel().collect(Collectors.toList()))
+                .collect(Collectors.toList());
+        return splitList;
+    }
+
+
+}
