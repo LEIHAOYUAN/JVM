73fe6c00917fa124513db1656ed4adbf27a4cabc
haoyuan.lei <646914508@qq.com>
2022-11-11 17:22:56 +0800

    MessageFormat单引号转义

---
+++ utils/src/main/java/com/lei/jvm/utils/base/utils/message/MessageFormatUtil.java
@@ -0,0 +1,20 @@
+package com.lei.jvm.utils.base.utils.message;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.MessageFormat;
+
+/**
+ *  职能描述：
+ *  @author leihaoyuan
+ *  @version 2022/11/11 13:37
+ */
+@Slf4j
+public class MessageFormatUtil {
+
+    public static void main(String[] args) {
+        log.info(MessageFormat.format("数据关联字段：[{0}]，未选择业务对象", "test", "AAA"));
+        log.info(MessageFormat.format("数据关联字段：''{0}''，未选择业务对象", "test"));
+        log.info(MessageFormat.format("{0}字段已启用不能删除", "test"));
+    }
+}
