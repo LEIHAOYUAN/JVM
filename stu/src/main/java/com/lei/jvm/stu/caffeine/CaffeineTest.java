ca34607438cccc7821a2841a46a85716a237aaa1
haoyuan.lei <646914508@qq.com>
2022-11-29 19:09:47 +0800

    验证caffeine中的引用缓存不能修改

--- stu/src/main/java/com/lei/jvm/stu/caffeine/CaffeineTest.java
+++ stu/src/main/java/com/lei/jvm/stu/caffeine/CaffeineTest.java
@@ -5,6 +5,9 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.RemovalCause;
 import com.github.benmanes.caffeine.cache.RemovalListener;
+import com.lei.jvm.stu.clone.Student;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -18,31 +21,38 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 public class CaffeineTest {
 
     public static void main(String[] args) {
-        testCache();
+        new CaffeineTest().testCache();
     }
 
-    private static void testCache() {
+    private void testCache() {
         String item = "AAAAAAAA";
-        Cache<Object, Object> cache = buildCache();
-        for (int i = 1; i <= 5000; i++) {
-            cache.put(i, item + i);
+        Cache<Object, People> cache = buildCache();
+        for (int i = 1; i < 5000; i++) {
+            cache.put(i, new People(item + i));
         }
-        cache.put(1, item + 1);
-        cache.put(1, item + 2);
-        cache.put(1, item + 3);
-        log.info("查询缓存结果：{}", JSON.toJSONString(cache.getIfPresent(1)));
+        People o1 = cache.getIfPresent(1);
+        log.info("查询缓存结果1：{}", JSON.toJSONString(o1));
+        o1.setName("XXXXX");
+        Object o2 = cache.getIfPresent(1);
+        log.info("查询缓存结果2：{}", JSON.toJSONString(o2));
     }
 
 
     private static Cache buildCache() {
         return Caffeine.newBuilder()
                 //设置缓存的 Entries 个数最多不超过1000个
-                .maximumSize(1).removalListener(new RemovalListener<Object, Object>() {
+                .maximumSize(5000).removalListener(new RemovalListener<Object, Object>() {
                     @Override
                     public void onRemoval(@Nullable Object key, @Nullable Object value, @NonNull RemovalCause removalCause) {
                         log.info("key={}已被移除", key);
                     }
                 }).build();
+    }
+
+    @Data
+    @AllArgsConstructor
+    public  class People{
+        private String name;
     }
 
 
