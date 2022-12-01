d78129386842c6c9b419a675606b569bf30b464b
haoyuan.lei <646914508@qq.com>
2022-12-01 16:44:16 +0800

    测试转换

--- stu/src/main/java/com/lei/jvm/stu/cast/Test.java
+++ stu/src/main/java/com/lei/jvm/stu/cast/Test.java
@@ -1,13 +1,10 @@
 package com.lei.jvm.stu.cast;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.google.common.collect.Lists;
-import com.lei.jvm.stu.clone.Student;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * @Author leihaoyuan
@@ -19,20 +16,35 @@ import java.util.Map;
 public class Test {
 
     public static void main(String[] args) {
-        Object obj = Lists.newArrayList("AAA", "BBB", 888, new Student(999));
+        Son son = new Son("AAA");
+        String jsonSon = JSON.toJSONString(son);
 
-        List list = (List) obj;
-        for (Object o : list) {
-            log.info("遍历：{}", JSON.toJSONString(o));
-        }
-        log.info("----------------------------------------------------------");
-        Student student = new Student(999);
-        student.setStudent(new Student(88888));
-        JSONObject jsonObject = JSON.parseObject(JSON.toJSONString(student));
-        for (Map.Entry<String, Object> entry : jsonObject.entrySet()) {
-            log.info("键：{}，值：{}", entry.getKey(), JSON.toJSONString(entry.getValue()));
-        }
+        String s = (String) JSON.parseObject(jsonSon, Object.class);
+
+
     }
 
+}
+
+enum Type {
+    SIMPLE(String.class);
+    @Getter
+    Class clazz;
+
+    Type(Class clazz) {
+        this.clazz = clazz;
+    }
+}
+
+@Data
+class Father {
 
 }
+
+@Data
+@AllArgsConstructor
+class Son extends Father {
+    private String name;
+}
+
+
