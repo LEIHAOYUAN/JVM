b8dfe2bda65e7ef4992989601eb2a78472f95256
haoyuan.lei <646914508@qq.com>
2023-02-01 17:36:41 +0800

    test

--- stu/src/main/java/com/lei/jvm/stu/collection/map/pojo/Test.java
+++ stu/src/main/java/com/lei/jvm/stu/collection/map/pojo/Test.java
@@ -15,7 +15,8 @@ import java.util.Map;
 public class Test {
 
     public static void main(String[] args) {
-        parseStruct();
+//        parseStruct();
+        log.info("比较结果={}","fff".equals(null));
     }
 
     private static void parseStruct() {
