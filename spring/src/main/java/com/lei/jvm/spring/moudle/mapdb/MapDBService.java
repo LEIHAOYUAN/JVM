1262ae0fddf394b817f21ac15b0dd6be6d41c41b
haoyuan.lei <646914508@qq.com>
2023-02-07 13:21:44 +0800

    MapDB依赖引入

---
+++ spring/src/main/java/com/lei/jvm/spring/moudle/mapdb/MapDBService.java
@@ -0,0 +1,14 @@
+package com.lei.jvm.spring.moudle.mapdb;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *  职能描述：mapdb服务
+ *  @author leihaoyuan
+ *  @version 2023/2/7 10:48
+ */
+@Service
+public class MapDBService {
+
+
+}
