1d1b0b954053f5ec6e6fc3242600c69f2d44f2fb
haoyuan.lei <646914508@qq.com>
2023-02-08 17:47:18 +0800

    mapdb工具类

---
+++ spring/src/main/java/com/lei/jvm/spring/utils/MapDBUtil.java
@@ -0,0 +1,30 @@
+package com.lei.jvm.spring.utils;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ *  职能描述：mapdb工具包
+ *  @author leihaoyuan
+ *  @version 2023/2/8 15:09
+ *  https://tool.4xseo.com/article/380951.html
+ */
+@UtilityClass
+public class MapDBUtil {
+
+    public static void heapDB() {
+
+    }
+
+    public static void offHeapDB() {
+//        DB offHeapDb = DBMaker.memoryDB().make();
+//        dbList.add(offHeapDb);
+//        ConcurrentMap offHeapDbMap = offHeapDb.hashMap("map").createOrOpen();
+//        testMap.put("堆外map", offHeapDbMap);
+    }
+
+    public static void diskDB() {
+
+    }
+
+
+}
