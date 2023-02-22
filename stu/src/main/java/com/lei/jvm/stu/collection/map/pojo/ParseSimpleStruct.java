788460d7c6399e299b8d8cb0d827f478d446ee9d
haoyuan.lei <646914508@qq.com>
2023-02-22 13:11:10 +0800

    递归解析嵌套层级struct

---
+++ stu/src/main/java/com/lei/jvm/stu/collection/map/pojo/ParseSimpleStruct.java
@@ -0,0 +1,85 @@
+package com.lei.jvm.stu.collection.map.pojo;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Stack;
+
+@Slf4j
+public class ParseSimpleStruct {
+
+
+    public static void main(String[] args) {
+        parseStruct();
+    }
+
+    private static void parseStruct() {
+        Struct a = buildStruct("a");
+        Struct a1 = buildStruct("a1");
+        Struct a2 = buildStruct("a2");
+
+        Struct a11 = buildStruct("a11");
+        Struct a12 = buildStruct("a12");
+
+        Struct a21 = buildStruct("a21");
+
+        Struct a111 = buildStruct("a111");
+        Struct a112 = buildStruct("a112");
+
+        a.setChildren(Lists.newArrayList(a1, a2));
+        a1.setChildren(Lists.newArrayList(a11, a12));
+        a2.setChildren(Lists.newArrayList(a21));
+
+        a11.setChildren(Lists.newArrayList(a111, a112));
+
+        log.info("嵌套对象结构={}", JSON.toJSONString(a));
+
+        Map<Object, Object> objectObjectMap = buildStructMap(a);
+        log.info("嵌套层级解析结果={}", JSON.toJSONString(objectObjectMap));
+
+        log.info("简单层级解析结果={}", JSON.toJSONString(buildStructMap(buildStruct("A"))));
+    }
+
+    private static Map<Object, Object> buildStructMap(Struct a) {
+        Map<Object, Object> father = new LinkedHashMap<Object, Object>();
+        Stack<Map<Object, Object>> stack = new Stack<Map<Object, Object>>();
+        buildStack(stack, a);
+        father.put(a.getKey(), stack.pop());
+        return stack.pop();
+    }
+
+    private static void buildStack(Stack<Map<Object, Object>> stack, Struct struct) {
+        Map<Object, Object> sun = new HashMap<>();
+        Map<Object, Object> father = new HashMap<>();
+        Map<Object, Object> unionMap = new HashMap<>();
+        if (CollectionUtils.isEmpty(struct.getChildren())) {
+            sun.put(struct.getKey(), struct.getValue());
+            stack.push(sun);
+        } else {
+            for (Struct child : struct.getChildren()) {
+                buildStack(stack, child);
+                father.put(child.getKey(), stack.pop());
+            }
+            unionMap.put(struct.getKey(), father);
+            stack.push(unionMap);
+        }
+
+        if (MapUtils.isNotEmpty(father)) {
+            stack.push(father);
+        }
+    }
+
+    private static Struct buildStruct(String key) {
+        Struct result = new Struct();
+        result.setKey(key);
+        result.setValue("name是" + key);
+        return result;
+    }
+
+}
--- stu/src/main/java/com/lei/jvm/stu/collection/map/pojo/ParseStruct.java
+++ stu/src/main/java/com/lei/jvm/stu/collection/map/pojo/ParseStruct.java
@@ -3,11 +3,11 @@ package com.lei.jvm.stu.collection.map.pojo;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
@@ -20,55 +20,69 @@ public class ParseStruct {
     }
 
     private static void parseStruct() {
-        Struct a = buildStruct("a");
-        Struct a1 = buildStruct("a1");
-        Struct a2 = buildStruct("a2");
+        Struct a = buildStruct("a", StructTypeEnum.OBJECT);
+        Struct a1 = buildStruct("a1", StructTypeEnum.OBJECT);
+        Struct a2 = buildStruct("a2", StructTypeEnum.ARRAY);
 
-        Struct a11 = buildStruct("a11");
-        Struct a12 = buildStruct("a12");
+        Struct a11 = buildStruct("a11", StructTypeEnum.ARRAY);
+        Struct a12 = buildStruct("a12", StructTypeEnum.BASE);
 
-        Struct a21 = buildStruct("a21");
+        Struct a21 = buildStruct("a21", StructTypeEnum.OBJECT);
 
-        Struct a111 = buildStruct("a111");
-        Struct a112 = buildStruct("a112");
+        Struct a111 = buildStruct("a111",StructTypeEnum.OBJECT);
+        Struct a112 = buildStruct("a112",StructTypeEnum.BASE);
+
+        Struct a211 = buildStruct("a211", StructTypeEnum.BASE);
+
+        Struct a1111 = buildStruct("a1111",StructTypeEnum.BASE);
+        Struct a1112 = buildStruct("a1112",StructTypeEnum.BASE);
 
         a.setChildren(Lists.newArrayList(a1, a2));
+        // a1节点
         a1.setChildren(Lists.newArrayList(a11, a12));
-        a2.setChildren(Lists.newArrayList(a21));
-
         a11.setChildren(Lists.newArrayList(a111, a112));
+        a111.setChildren(Lists.newArrayList(a1111,a1112));
+        // a2节点
+        a2.setChildren(Lists.newArrayList(a21));
+        a21.setChildren(Lists.newArrayList(a211));
+
 
         log.info("嵌套对象结构={}", JSON.toJSONString(a));
 
-        Map<Object, Object> objectObjectMap = buildStructMap(a);
+        Map<String, Object> objectObjectMap = buildStructMap(a);
         log.info("嵌套层级解析结果={}", JSON.toJSONString(objectObjectMap));
-
-        log.info("简单层级解析结果={}", JSON.toJSONString(buildStructMap(buildStruct("A"))));
     }
 
-    private static Map<Object, Object> buildStructMap(Struct a) {
-        Map<Object, Object> father = new LinkedHashMap<Object, Object>();
-
-        Stack<Map<Object, Object>> stack = new Stack<Map<Object, Object>>();
-        buildStack(stack, a);
-        father.put(a.getKey(), stack.pop());
-        return father;
+    private static Map<String, Object> buildStructMap(Struct struct) {
+        if (null == struct || StructTypeEnum.OBJECT != struct.getStructType()) {
+            throw new IllegalArgumentException("非法结构体");
+        }
+        Stack<Object> stack = new Stack<>();
+        buildStack(stack, struct);
+        return (Map<String, Object>) stack.pop();
     }
 
-    private static void buildStack(Stack<Map<Object, Object>> stack, Struct struct) {
-        Map<Object, Object> sun = new HashMap<>();
-        Map<Object, Object> father = new HashMap<>();
-        Map<Object, Object> unionMap = new HashMap<>();
-        if (CollectionUtils.isEmpty(struct.getChildren())) {
-            sun.put(struct.getKey(), struct.getValue());
-            stack.push(sun);
-        } else {
+    private static void buildStack(Stack<Object> stack, Struct struct) {
+        Map<String, Object> father = new HashMap<>();
+        Map<String, Object> unionMap = new HashMap<>();
+        if (StructTypeEnum.BASE == struct.getStructType()) {
+            stack.push(struct.getValue());
+        } else if (StructTypeEnum.ARRAY == struct.getStructType()) {
+            List<Struct> children = struct.getChildren();
+            List<Object> list = new ArrayList<>(10);
+            for (Struct child : children) {
+                buildStack(stack, child);
+                list.add(stack.pop());
+            }
+            unionMap.put(struct.getKey(), list);
+            stack.push(unionMap);
+        } else if (StructTypeEnum.OBJECT == struct.getStructType()) {
+            Map<String, Object> map = new HashMap<>();
             for (Struct child : struct.getChildren()) {
                 buildStack(stack, child);
-                father.put(child.getKey(), stack.pop());
+                map.put(child.getKey(), stack.pop());
             }
-            unionMap.put(struct.getKey(), father);
-            stack.push(unionMap);
+            father.put(struct.getKey(), map);
         }
 
         if (MapUtils.isNotEmpty(father)) {
@@ -76,11 +90,12 @@ public class ParseStruct {
         }
     }
 
-    private static Struct buildStruct(String key) {
-        Struct result = new Struct();
-        result.setKey(key);
-        result.setValue("name是" + key);
-        return result;
+    private static Struct buildStruct(String key, StructTypeEnum type) {
+        Struct struct = new Struct();
+        struct.setKey(key);
+        struct.setValue("name是" + key);
+        struct.setStructType(type);
+        return struct;
     }
 
 }
