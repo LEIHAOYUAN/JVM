b1b4f38157c77865cbb8987ebfa1253f8892519b
haoyuan.lei <646914508@qq.com>
2023-03-01 09:15:38 +0800

    根据对象属性去重集合元素

--- stu/src/main/java/com/lei/jvm/stu/stream/CollectingAndThenTest.java
+++ stu/src/main/java/com/lei/jvm/stu/stream/CollectingAndThenTest.java
@@ -3,6 +3,7 @@ package com.lei.jvm.stu.stream;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -11,22 +12,24 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @Description
+ * @Description 根据对象属性去重并保留最旧数据
  * @Author leihaoyuan
  * @Date 2020/11/16 15:52
  */
+@Slf4j
 public class CollectingAndThenTest {
 
     public static void main(String[] args) {
         List<Student> list = Lists.newArrayList();
-        list.add(new Student("AAA", "20", BigDecimal.TEN));
-        list.add(new Student("AAA", "20", BigDecimal.TEN));
-        list.add(new Student("AAA", "20", BigDecimal.TEN));
-        list.add(new Student("AAA", "20", BigDecimal.TEN));
-        list.add(new Student("AAA", "20", BigDecimal.TEN));
-        list.add(new Student("AAAA", "20", BigDecimal.TEN));
-        list.add(new Student("BB", "10", BigDecimal.TEN));
-        ArrayList<Student> distinctList = list.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> Sets.newTreeSet(Comparator.comparing(item -> item.getName() + ";" + item.getAge()))), ArrayList::new));
-        System.out.println(JSON.toJSONString(distinctList));
+        list.add(new Student("AAA", "1", BigDecimal.TEN));
+        list.add(new Student("AAA", "2", BigDecimal.TEN));
+        list.add(new Student("AAA", "3", BigDecimal.TEN));
+        list.add(new Student("AAA", "4", BigDecimal.TEN));
+        list.add(new Student("AAA", "5", BigDecimal.TEN));
+        list.add(new Student("AAAA", "60", BigDecimal.TEN));
+        list.add(new Student("BB", "999", BigDecimal.TEN));
+//        ArrayList<Student> distinctList = list.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> Sets.newTreeSet(Comparator.comparing(item -> item.getName() + ";" + item.getAge()))), ArrayList::new));
+        ArrayList<Student> distinctList = list.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> Sets.newTreeSet(Comparator.comparing(Student::getName))), ArrayList::new));
+        log.info(JSON.toJSONString(distinctList));
     }
 }
