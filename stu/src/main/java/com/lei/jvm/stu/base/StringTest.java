3bde5cc995db0e2aacd829da1b3a90db5799ac72
haoyuan.lei <646914508@qq.com>
2023-04-13 14:00:14 +0800

    测试方法重载

--- stu/src/main/java/com/lei/jvm/stu/base/StringTest.java
+++ stu/src/main/java/com/lei/jvm/stu/base/StringTest.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
-import org.apache.xmlbeans.impl.xb.xsdschema.ListDocument;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -26,29 +25,38 @@ public class StringTest {
     private static final String ESCAPE_POINT = "\\.";
 
     public static void main(String[] args) {
-        testExtract();
+//        testExtract();
+        getStr("A","测试");
     }
 
-    public static void testContains(){
+    public static void getStr(String key, String defaultContent) {
+        log.info("默认方法调用");
+    }
+
+    public static void getStr(String langKey, Object... args) {
+        log.info("多参数方法调用");
+    }
+
+    public static void testContains() {
         String param = "中文简体(aaa-zh-zn)";
-        log.info("测试结果={}","zh-zn".contains(param));
-        log.info("测试结果={}",param.contains("zh-zn"));
+        log.info("测试结果={}", "zh-zn".contains(param));
+        log.info("测试结果={}", param.contains("zh-zn"));
     }
 
-    public static void testExtract(){
+    public static void testExtract() {
         String param = "@aaa[中国+世界]@bbb@";
-        log.info("warp方法测试={}",StrUtil.isWrap(param,"[","]"));
+        log.info("warp方法测试={}", StrUtil.isWrap(param, "[", "]"));
         String[] split = param.split("@");
         log.info(split.toString());
-        log.info("instance测试{}",null instanceof List);
+        log.info("instance测试{}", null instanceof List);
 
     }
 
 
-    public static void testSubstring(){
+    public static void testSubstring() {
         String param = "aa.bb.";
-        String substring = param.substring(0,param.lastIndexOf("."));
-        log.info("截取结果={}",substring);
+        String substring = param.substring(0, param.lastIndexOf("."));
+        log.info("截取结果={}", substring);
     }
 
     public static void testSplit() {
@@ -114,8 +122,6 @@ public class StringTest {
         String collect = param.stream().map(String::valueOf).collect(Collectors.joining(","));
         log.info("逗号拼接结果：{}", collect);
     }
-
-
 
 
 }
