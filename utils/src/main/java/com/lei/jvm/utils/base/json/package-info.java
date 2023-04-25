9557b0a10d225d48b5d1ebcddf7ce8cd40be9222
haoyuan.lei <646914508@qq.com>
2023-04-25 13:16:45 +0800

    JSON schema

---
+++ utils/src/main/java/com/lei/jvm/utils/base/json/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *  职能描述：JSON-Schema
+ *  @author leihaoyuan
+ *  @version 2023/4/25 10:58
+ *  【json-schema】
+ *  https://www.cnblogs.com/terencezhou/p/10474617.html
+ */
+package com.lei.jvm.utils.base.json;
