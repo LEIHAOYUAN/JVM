d2d8bf17654fd4f387137cd4f9620634cdba8283
haoyuan.lei <646914508@qq.com>
2023-04-25 19:52:40 +0800

    mapdb测试

---
+++ spring/src/test/java/com/lei/jvm/mapdb/MapDBTest.java
@@ -0,0 +1,54 @@
+package com.lei.jvm.mapdb;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.Serializer;
+
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ *  职能描述：
+ *  @author leihaoyuan
+ *  @version 2023/4/25 19:40
+ */
+@Slf4j
+public class MapDBTest {
+
+    private static final String FILE_DB = "file.mapdb";
+
+
+    @Test
+    public void testDirectMemory() {
+        DB db = DBMaker.memoryDB().make();
+        ConcurrentMap map = db.hashMap("map").createOrOpen();
+        String key = "Hello";
+        String val = "simple";
+        map.put(key, val);
+        log.info("获取内容={}",map.get(key));
+    }
+
+
+    @Test
+    public void testFile() {
+        DB db = DBMaker.fileDB(FILE_DB).make();
+        ConcurrentMap map = db.hashMap("map").createOrOpen();
+        String key = "something";
+        map.put(key, "here");
+        log.info("文件读取={}", map.get(key));
+        db.close();
+    }
+
+    @Test
+    public void testMixMemory() {
+        DB db = DBMaker.fileDB(FILE_DB).fileMmapEnable().make();
+        ConcurrentMap<String,String> map = db.hashMap("testMap", Serializer.STRING, Serializer.STRING).createOrOpen();
+        String key = "name";
+        map.put(key, "xiaobai");
+        log.info("获取内容={}",map.get(key));
+        db.close();
+    }
+
+
+}
