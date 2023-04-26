605c5eedba0bd7ce5ecab231452b02ed6f7289d5
haoyuan.lei <646914508@qq.com>
2023-04-26 11:28:47 +0800

    mapdb性能测试

--- spring/src/test/java/com/lei/jvm/mapdb/MapDBTest.java
+++ spring/src/test/java/com/lei/jvm/mapdb/MapDBTest.java
@@ -6,6 +6,7 @@ import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.mapdb.Serializer;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 
 /**
@@ -19,14 +20,17 @@ public class MapDBTest {
 
     private static final String FILE_DB = "file.mapdb";
 
+    private static final int count = 10000;
+
+    private static String key = "key";
+
     @Test
     public void testJvmMemory() {
         DB db = DBMaker.memoryDB().make();
         ConcurrentMap map = db.hashMap("map").createOrOpen();
-        String key = "Hello";
-        String val = "simple";
-        map.put(key, val);
-        log.info("获取内容={}", map.get(key));
+        writeData(map);
+        readData(map);
+        db.close();
     }
 
 
@@ -34,10 +38,9 @@ public class MapDBTest {
     public void testDirectMemory() {
         DB db = DBMaker.memoryDirectDB().make();
         ConcurrentMap map = db.hashMap("direct_map").createOrOpen();
-        String key = "Hello";
-        String val = "simple";
-        map.put(key, val);
-        log.info("获取内容={}", map.get(key));
+        writeData(map);
+        readData(map);
+        db.close();
     }
 
 
@@ -45,15 +48,8 @@ public class MapDBTest {
     public void testFile() {
         DB db = DBMaker.fileDB(FILE_DB).make();
         ConcurrentMap map = db.hashMap("map").createOrOpen();
-        String key = "something";
-
-        map.put(key, "here123456");
-        map.put(key, "here1234560000");
-        map.put(key, "here123456111111111");
-        for (int i = 0; i < 10000; i++) {
-            map.put(key + i, "value".concat(key));
-        }
-        log.info("文件读取={}", map.get(key));
+        writeData(map);
+        readData(map);
         db.close();
     }
 
@@ -61,10 +57,25 @@ public class MapDBTest {
     public void testMixMemory() {
         DB db = DBMaker.fileDB(FILE_DB).fileMmapEnable().make();
         ConcurrentMap<String, String> map = db.hashMap("testMap", Serializer.STRING, Serializer.STRING).createOrOpen();
-        String key = "name";
-        map.put(key, "xiaobai");
-        log.info("获取内容={}", map.get(key));
+        writeData(map);
+        readData(map);
         db.close();
+    }
+
+    private void writeData(Map<String, String> map) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            map.put(key + i, "value".concat(key));
+        }
+        log.info("写[{}]次耗时={}毫秒", count, System.currentTimeMillis() - start);
+    }
+
+    private void readData(Map<String, String> map) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            log.info("读取[{}]内容[{}]", key + i, map.get(key + i));
+        }
+        log.info("读[{}]次耗时={}毫秒", count, System.currentTimeMillis() - start);
     }
 
 
