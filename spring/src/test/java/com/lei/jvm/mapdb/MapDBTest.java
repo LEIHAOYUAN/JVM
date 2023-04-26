2f840b95bcac526dd771a28fc3645e5ef0a2a017
haoyuan.lei <646914508@qq.com>
2023-04-26 17:34:10 +0800

    mapdb性能测试

--- spring/src/test/java/com/lei/jvm/mapdb/MapDBTest.java
+++ spring/src/test/java/com/lei/jvm/mapdb/MapDBTest.java
@@ -7,29 +7,28 @@ import org.mapdb.DBMaker;
 import org.mapdb.Serializer;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
 
 /**
- *  职能描述：
+ *  职能描述：mapdb性能测试
  *  @author leihaoyuan
- *  @version 2023/4/25 19:40
- *  https://www.jianshu.com/p/b6f43302338e
+ *  @version 2023/4/26 17:20
  */
 @Slf4j
 public class MapDBTest {
 
-    private static final String FILE_DB = "file.mapdb";
+    private static final String FILE_DB = "tips.cache";
 
-    private static final int count = 10000;
+    private static final int count = 100000;
 
-    private static String key = "key";
+    private static final String key = "key";
+
+    private static final String MAP = "test-map";
 
     @Test
     public void testJvmMemory() {
         DB db = DBMaker.memoryDB().make();
-        ConcurrentMap map = db.hashMap("map").createOrOpen();
-        writeData(map);
-        readData(map);
+        writeData(db);
+        readData(db);
         db.close();
     }
 
@@ -37,45 +36,47 @@ public class MapDBTest {
     @Test
     public void testDirectMemory() {
         DB db = DBMaker.memoryDirectDB().make();
-        ConcurrentMap map = db.hashMap("direct_map").createOrOpen();
-        writeData(map);
-        readData(map);
+        writeData(db);
+        readData(db);
+        db.close();
+    }
+
+    @Test
+    public void testFileNormal() {
+        DB db = DBMaker.fileDB(FILE_DB).fileDeleteAfterClose().closeOnJvmShutdown().make();
+        writeData(db);
+        readData(db);
         db.close();
     }
 
 
     @Test
-    public void testFile() {
+    public void testFileChannel() {
+        DB db = DBMaker.fileDB(FILE_DB).fileChannelEnable().fileDeleteAfterClose().closeOnJvmShutdown().make();
+        writeData(db);
+        readData(db);
+        db.close();
+    }
+
+
+    @Test
+    public void testFileMMapSupported() {
+        DB db = DBMaker.fileDB(FILE_DB).fileMmapEnableIfSupported().fileDeleteAfterClose().closeOnJvmShutdown().make();
+        writeData(db);
+        readData(db);
+        db.close();
+    }
+
+    @Test
+    public void testFileMMap() {
         DB db = DBMaker.fileDB(FILE_DB).fileMmapEnable().make();
-        ConcurrentMap<String, String> localMap = db.hashMap("local-map", Serializer.STRING, Serializer.STRING).createOrOpen();
-        ConcurrentMap<String, String> remoteMap = db.hashMap("remote-map", Serializer.STRING, Serializer.STRING).createOrOpen();
-        String key = "AAA";
-        localMap.put(key, "local-map-value");
-        remoteMap.put(key, "remote-map-value");
-        log.info("本地内容={}", localMap.get(key));
-        log.info("远程内容={}", remoteMap.get(key));
+        writeData(db);
+        readData(db);
         db.close();
     }
 
-    @Test
-    public void testFileAutoDelete() {
-        DB db = DBMaker.fileDB(FILE_DB).fileMmapEnable().fileDeleteAfterClose().make();
-        ConcurrentMap map = db.hashMap("map").createOrOpen();
-        writeData(map);
-        readData(map);
-        db.close();
-    }
-
-    @Test
-    public void testMixMemory() {
-        DB db = DBMaker.fileDB(FILE_DB).fileMmapEnable().make();
-        ConcurrentMap<String, String> map = db.hashMap("testMap", Serializer.STRING, Serializer.STRING).createOrOpen();
-        writeData(map);
-        readData(map);
-        db.close();
-    }
-
-    private void writeData(Map<String, String> map) {
+    private void writeData(DB db) {
+        Map<String, String> map = db.hashMap(MAP, Serializer.STRING, Serializer.STRING).createOrOpen();
         long start = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
             map.put(key + i, "new-value".concat(key));
@@ -83,7 +84,8 @@ public class MapDBTest {
         log.info("写[{}]次耗时={}毫秒", count, System.currentTimeMillis() - start);
     }
 
-    private void readData(Map<String, String> map) {
+    private void readData(DB db) {
+        Map<String, String> map = db.hashMap(MAP, Serializer.STRING, Serializer.STRING).createOrOpen();
         long start = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
             log.info("读取[{}]内容[{}]", key + i, map.get(key + i));
