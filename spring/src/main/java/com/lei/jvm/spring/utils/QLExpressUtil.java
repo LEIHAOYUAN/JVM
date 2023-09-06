75904913282683e1ef77a0c9e1766cbb89c4af69
haoyuan.lei <646914508@qq.com>
2023-09-06 13:52:03 +0800

    QLExpress自定义函数测试

--- spring/src/main/java/com/lei/jvm/spring/utils/QLExpressUtil.java
+++ spring/src/main/java/com/lei/jvm/spring/utils/QLExpressUtil.java
@@ -1,14 +1,50 @@
 package com.lei.jvm.spring.utils;
 
-import lombok.experimental.UtilityClass;
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+import com.ql.util.express.IExpressContext;
+import com.ql.util.express.Operator;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * 阿里巴巴QLExpress表达式工具类
+ *
  * @author leihaoyuan
  * @version 2023/9/6 10:54
  */
 @Slf4j
-@UtilityClass
 public class QLExpressUtil {
+
+    private ExpressRunner runner;
+    private IExpressContext context;
+
+    public QLExpressUtil() {
+        runner = new ExpressRunner();
+        context = new DefaultContext();
+    }
+
+    public void addFunction(String functionName) {
+        try {
+
+            runner.addFunction(functionName, new Operator() {
+                @Override
+                public Object executeInner(Object[] objects) throws Exception {
+                    return null;
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Object executeExpression(String expression) {
+        try {
+            return runner.execute(expression, context, null, true, false);
+        } catch (Exception e) {
+            log.error("执行异常表达式=[{}],异常={}", expression, e.getMessage(), e);
+            return null;
+        }
+    }
+
+
 }
