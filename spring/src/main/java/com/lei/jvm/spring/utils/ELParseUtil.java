4777fa74801b84f7e76fd813bbd1253322d2dece
haoyuan.lei <646914508@qq.com>
2023-09-06 10:59:47 +0800

    表达式解析工具类

---
+++ spring/src/main/java/com/lei/jvm/spring/utils/ELParseUtil.java
@@ -0,0 +1,23 @@
+package com.lei.jvm.spring.utils;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * EL表达式解析工具
+ * @author leihaoyuan
+ * @version 2023/9/6 10:52
+ */
+@Slf4j
+@UtilityClass
+public class ELParseUtil {
+
+
+
+
+
+
+
+
+
+}
---
+++ spring/src/main/java/com/lei/jvm/spring/utils/QLExpressUtil.java
@@ -0,0 +1,14 @@
+package com.lei.jvm.spring.utils;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 阿里巴巴QLExpress表达式工具类
+ * @author leihaoyuan
+ * @version 2023/9/6 10:54
+ */
+@Slf4j
+@UtilityClass
+public class QLExpressUtil {
+}
