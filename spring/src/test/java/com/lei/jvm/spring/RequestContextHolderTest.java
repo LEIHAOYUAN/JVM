666301b35ccfbd18da688b3f0b49c3c6599d8d18
haoyuan.lei <646914508@qq.com>
2023-09-06 11:35:32 +0800

    request上下文工具类封装

---
+++ spring/src/test/java/com/lei/jvm/spring/RequestContextHolderTest.java
@@ -0,0 +1,36 @@
+package com.lei.jvm.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author leihaoyuan
+ * @version 2023/9/6 11:27
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class RequestContextHolderTest {
+
+
+    @Test
+    public void testRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest();
+
+        log.info("请求上下文参数={}", requestAttributes);
+        log.info("请求上下文参数={}", request);
+    }
+
+
+}
