63c3492bb3b3bbc7cc12305f523a76a2b81351fc
haoyuan.lei <646914508@qq.com>
2023-10-12 11:11:11 +0800

    封装okhttp工具包

---
+++ utils/src/main/java/com/lei/jvm/utils/base/utils/http/okhttp/OKHttpUtils.java
@@ -0,0 +1,75 @@
+package com.lei.jvm.utils.base.utils.http.okhttp;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+@Slf4j
+@UtilityClass
+public class OKHttpUtils {
+    private static final MediaType JSON_TYPE = MediaType.parse("application/json; charset=utf-8");
+    private static OkHttpClient client;
+
+    static {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                    }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+
+            SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            client = new OkHttpClient.Builder()
+                    .sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0])
+                    .hostnameVerifier((hostname, session) -> true)
+                    .build();
+        } catch (Exception e) {
+            log.error("初始化SSL异常={}", e.getMessage(), e);
+        }
+    }
+
+    public static String sendGetRequest(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public static String sendPostRequest(String url, String jsonBody) throws IOException {
+        RequestBody body = RequestBody.create(jsonBody, JSON_TYPE);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
