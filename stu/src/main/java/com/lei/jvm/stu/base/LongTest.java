929dc600108e176f2b57dcbc01e480e81da4d13d
haoyuan.lei <646914508@qq.com>
2023-10-23 19:10:59 +0800

    字符串集合过滤long类型值

---
+++ stu/src/main/java/com/lei/jvm/stu/base/LongTest.java
@@ -0,0 +1,35 @@
+package com.lei.jvm.stu.base;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+/**
+ * @author leihaoyuan
+ * @version 2023/10/23 18:54
+ */
+@Slf4j
+public class LongTest {
+
+    public static void main(String[] args) {
+        parseLong();
+    }
+
+
+    private static void parseLong() {
+        List<Long> result = Lists.newArrayList();
+        List<String> param = Lists.newArrayList("AAA", "123456888888", "_NONE_");
+        for (String item : param) {
+            try {
+                result.add(Long.parseLong(item));
+            } catch (Exception ex) {
+                log.warn("过滤非自定义数据权限项=[{}]", item);
+            }
+        }
+        log.info("自定义权限配置结果={}", JSON.toJSONString(result));
+    }
+
+
+}
