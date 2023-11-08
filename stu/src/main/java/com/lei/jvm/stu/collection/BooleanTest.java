3dc9955598a82dd83dd4514f5747c1b2f090146f
haoyuan.lei <646914508@qq.com>
2023-11-08 10:17:59 +0800

    测试空boolean的equals

---
+++ stu/src/main/java/com/lei/jvm/stu/collection/BooleanTest.java
@@ -0,0 +1,23 @@
+package com.lei.jvm.stu.collection;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author leihaoyuan
+ * @version 2023/11/8 10:14
+ */
+@Slf4j
+public class BooleanTest {
+
+    public static void main(String[] args) {
+        testEquals();
+    }
+
+
+    public static void testEquals(){
+        Boolean param = null;
+        log.info("测试结果={}",Boolean.TRUE.equals(param));
+    }
+
+
+}
