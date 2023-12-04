fe2f9b7f54b0c286a5c2e8f62a6b6e3aee15000d
haoyuan.lei <646914508@qq.com>
2023-12-04 13:40:44 +0800

    测试kryo增加字段

--- utils/src/test/java/com/lei/jvm/utils/base/utils/KryoXUtilTest.java
+++ utils/src/test/java/com/lei/jvm/utils/base/utils/KryoXUtilTest.java
@@ -5,6 +5,8 @@ import com.lei.jvm.utils.base.utils.kryo.KryoXUtil;
 import lombok.Data;
 import org.junit.Test;
 
+import java.io.Serializable;
+
 /**
  * @author leihaoyuan
  * @version 2023/12/4 11:28
@@ -15,13 +17,16 @@ public class KryoXUtilTest {
     @Test
     public void testSerialize() {
         byte[] serialize = KryoXUtil.serialize(buildStudent());
-        System.out.println(("序列化结果=" + JSON.toJSONString(serialize)));
+        System.out.println(("序列化结果=" + JSON.toJSON(serialize)));
     }
 
 
     @Test
     public void testDeserialize() {
-        Student deserialize = KryoXUtil.deserialize(KryoXUtil.serialize(buildStudent()), Student.class);
+        // byte[] bytes = new byte[]{};
+        // byte[] bytes = new byte[]{1, 0, 99, 111, 109, 46, 108, 101, 105, 46, 106, 118, 109, 46, 117, 116, 105, 108, 115, 46, 98, 97, 115, 101, 46, 117, 116, 105, 108, 115, 46, 75, 114, 121, 111, 88, 85, 116, 105, 108, 84, 101, 115, 116, 36, 83, 116, 117, 100, 101, 110, -12, 1, 1, -58, 1, 1, -125, -27, -68, -96, -28, -72, -119};
+        byte[] bytes = new byte[]{1, 0, 99, 111, 109, 46, 108, 101, 105, 46, 106, 118, 109, 46, 117, 116, 105, 108, 115, 46, 98, 97, 115, 101, 46, 117, 116, 105, 108, 115, 46, 75, 114, 121, 111, 88, 85, 116, 105, 108, 84, 101, 115, 116, 36, 83, 116, 117, 100, 101, 110, -12, 1, 1, -58, 1, 1, -125, -27, -68, -96, -28, -72, -119, 1, 1};
+        Student deserialize = KryoXUtil.deserialize(bytes, Student.class);
         System.out.println(("反序列化结果=" + JSON.toJSONString(deserialize)));
     }
 
@@ -30,15 +35,19 @@ public class KryoXUtilTest {
         Student stu = new Student();
         stu.setName("张三");
         stu.setAge(99);
+//        stu.setSex(true);
         return stu;
     }
 
 
     @Data
-    public class Student {
+    public class Student implements Serializable {
+        private static final long serialVersionUID = -8670690954203461108L;
         private String name;
 
         private Integer age;
+
+//        private Boolean sex;
     }
 
 
