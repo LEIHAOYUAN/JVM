bafdd0173b5d530d705920dd8479594b36a09368
haoyuan.lei <646914508@qq.com>
2024-06-06 13:36:26 +0800

    生日数据加解密工具

---
+++ utils/src/main/java/com/lei/jvm/utils/base/utils/birthday/BirthdayUtil.java
@@ -0,0 +1,56 @@
+package com.lei.jvm.utils.base.utils.birthday;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ * @author leihaoyuan
+ * @version 2024/6/6 10:30
+ */
+@Slf4j
+@UtilityClass
+public class BirthdayUtil {
+
+    public static void main(String[] args) {
+        log.info("解密生日={}", decryptUint(3788187338L));
+        log.info("加密生日={}", encryptUint(20240606));
+    }
+
+    public static long decryptUint(long value) {
+        if (value < 99999999) {
+            return value;
+        }
+
+        value = (value & 0xFFFFFFF) ^ 0x151212;
+        byte[] bytes = new byte[8];
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.putLong(value);
+        byte b = bytes[2];
+        bytes[2] = bytes[1];
+        bytes[1] = bytes[0];
+        bytes[0] = b;
+        buffer.rewind();
+        value = buffer.getInt();
+        return value;
+    }
+
+    public static long encryptUint(long value) {
+        byte[] bytes = new byte[8];
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.putLong(value);
+        byte b = bytes[0];
+        bytes[0] = bytes[1];
+        bytes[1] = bytes[2];
+        bytes[2] = b;
+        buffer.rewind();
+        value = buffer.getInt();
+        return ((value ^ 0x151212) | 0xE0000000L);
+    }
+
+
+}
