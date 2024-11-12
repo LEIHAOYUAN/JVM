8332488073998b69881426e72c39c842b4446277
haoyuan.lei <646914508@qq.com>
2024-11-12 16:18:25 +0800

    网关签名算法完善

--- utils/src/main/java/com/lei/jvm/utils/base/utils/sign/MD5Util.java
+++ utils/src/main/java/com/lei/jvm/utils/base/utils/sign/MD5Util.java
@@ -1,51 +1,71 @@
 package com.lei.jvm.utils.base.utils.sign;
 
-import com.google.common.collect.Maps;
+
+import org.apache.commons.codec.Charsets;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Map;
 
 
 public class MD5Util {
 
-    public static void main(String[] args) {
-        // 业务参数
-        Map<String, Object> param = Maps.newHashMap();
-        param.put("tenantCode", "dev");
-        param.put("orgId", "123");
-        param.put("userId", "456");
-        // 加密结果:d3811ab6e2961bab51447cbccc4df2d5
-        String md5Result = doSign(param, "+LN8mKLg3ZcCAwEAAQ==");
-        System.out.println("MD5加密结果:" + md5Result);
+    /**
+     * 请求体加密算法
+     *
+     * @param body 请求体
+     * @return 加密后的请求体
+     */
+    public static String doSignBody(String body) {
+        if (StringUtils.isBlank(body)) {
+            return StringUtils.EMPTY;
+        }
+        return base64(org.apache.commons.codec.digest.DigestUtils.md5Hex(body.getBytes(Charsets.UTF_8)));
     }
 
-
-    public static String doSign(Map<String, Object> param, String secretKey) {
+    /**
+     * 整体请求参数加密算法
+     *
+     * @param method     请求方法
+     * @param uri        请求路径
+     * @param queryParam query参数
+     * @param headerStr  请求头
+     * @param body       请求体
+     * @param secretKey  密钥
+     * @return 签名结果
+     */
+    public static String doSign(String method, String uri, String queryParam, String headerStr, String body, String secretKey) {
         if (StringUtils.isBlank(secretKey)) {
             throw new IllegalArgumentException("MD5签名密钥不能为空");
         }
         // 1. 参数排序拼接处理
-        byte[] joinParam = ParamUtil.join(param, secretKey);
+        String joinParam = SignUtil.join(method, uri, queryParam, headerStr, body, secretKey);
         // 2. 生成MD5加密结果
         byte[] bytes = generateMD5(joinParam);
         // 3. 转换为十六进制字符串
         return bytesToHex(bytes);
     }
 
+    private static String base64(String param) {
+        if (null == param) {
+            throw new IllegalArgumentException("base64 param is null");
+        }
+        return new String(Base64.encodeBase64(param.getBytes()), Charsets.UTF_8);
+    }
+
     /**
      * 生成给定字节数组的MD5哈希值。
      *
-     * @param data 要计算其MD5哈希值的数据
+     * @param param 要计算其MD5哈希值的数据
      * @return 字节数组形式的MD5哈希值
      */
-    public static byte[] generateMD5(byte[] data) {
+    private static byte[] generateMD5(String param) {
         try {
             // 创建MessageDigest实例用于MD5哈希
             MessageDigest md = MessageDigest.getInstance("MD5");
             // 执行哈希计算
-            return md.digest(data);
+            return md.digest(param.getBytes(Charsets.UTF_8));
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("MD5 algorithm not found", e);
         }
@@ -68,6 +88,5 @@ public class MD5Util {
         }
         return hexString.toString();
     }
-
 
 }
--- utils/src/main/java/com/lei/jvm/utils/base/utils/sign/SignUtil.java
+++ utils/src/main/java/com/lei/jvm/utils/base/utils/sign/SignUtil.java
@@ -2,8 +2,13 @@ package com.lei.jvm.utils.base.utils.sign;
 
 import org.apache.commons.lang.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.StringJoiner;
 import java.util.TreeMap;
 
 
@@ -20,6 +25,46 @@ public class SignUtil {
         String join = join(buildHeader(signHeaderMap), "WXb0CAwEAAQ==");
         System.out.println("签名参数拼接示例:" + join);
     }
+
+    /**
+     * 拼接查询参数
+     * @param queryMap 查询参数
+     * @return 拼接后的值
+     */
+    public static String buildQueryParam(Map<String, String[]> queryMap) {
+        // 使用TreeMap自动排序键值
+        Map<String, String[]> sortedParameters = new TreeMap<>(queryMap);
+        StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry<String, String[]> entry : sortedParameters.entrySet()) {
+            String key = entry.getKey();
+            String[] values = entry.getValue();
+            String value;
+
+            if (values != null && values.length != 0) {
+                StringJoiner joiner = new StringJoiner(",");
+                for (String val : values) {
+                    if (StringUtils.isNotBlank(val)) {
+                        joiner.add(val);
+                    }
+                }
+                value = joiner.toString();
+            } else {
+                value = "";
+            }
+
+            if (StringUtils.isNotBlank(value)) {
+                sb.append(key).append('=').append(value);
+            } else {
+                sb.append(key);
+            }
+            sb.append('&');
+        }
+
+        // 移除最后一个多余的'&'
+        return sb.length() > 0 ? sb.substring(0, sb.length() - 1) : sb.toString();
+    }
+
 
 
     /**
@@ -41,7 +86,30 @@ public class SignUtil {
         return headers.substring(0, headers.lastIndexOf("\n"));
     }
 
-    private static String join(String signHeaderStr, String secretKey) {
+
+    /**
+     * 签名参数拼接
+     *
+     * @param method     请求方法
+     * @param uri        请求URI
+     * @param queryParam GET查询参数
+     * @param headerStr  签名请求头（只包含需要签名的header）
+     * @param encryptBody       加密后的请求体（详见：MD5Util.doSignBody）
+     * @param secretKey  密钥
+     * @return 签名字符串
+     */
+    public static String join(String method, String uri, String queryParam, String headerStr, String encryptBody, String secretKey) {
+        String connector = "\n";
+        String needSignParamString;
+        if (StringUtils.isNotBlank(queryParam)) {
+            needSignParamString = doJoin(connector, new String[]{queryParam, headerStr, encryptBody});
+        } else {
+            needSignParamString = doJoin(connector, new String[]{headerStr, encryptBody});
+        }
+        return doJoin(connector, method, uri, needSignParamString, secretKey);
+    }
+
+    public static String join(String signHeaderStr, String secretKey) {
         /**
          * 拼接说明：
          * [1]"\n":参数之间分隔符
