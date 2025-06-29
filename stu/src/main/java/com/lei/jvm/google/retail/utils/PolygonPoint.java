de8fba0a19e0c9ba3616f698d1dd0a317af0d9c8
Ryan Lei <ryanlei@xm.wonder.com>
2025-06-29 15:24:26 +0800

    geohash算法实现

---
+++ stu/src/main/java/com/lei/jvm/google/retail/utils/PolygonPoint.java
@@ -0,0 +1,14 @@
+package com.lei.jvm.google.retail.utils;
+
+/**
+ * @author ryan
+ */
+public class PolygonPoint {
+    public double latitude;
+    public double longitude;
+
+    public PolygonPoint(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
