e8b76cb3619ecd71cb15ad1268334b34b1dcfe05
Ryan Lei <ryanlei@xm.wonder.com>
2025-07-30 16:51:24 +0800

    test chufa

---
+++ stu/src/main/java/com/lei/jvm/stu/number/NumberTest.java
@@ -0,0 +1,15 @@
+package com.lei.jvm.stu.number;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author ryan
+ */
+@Slf4j
+public class NumberTest {
+
+    public static void main(String[] args) {
+        log.info("测试除法={}", 10 / 3);
+    }
+
+}
