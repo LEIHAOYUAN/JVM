efdcdb0194452d8257545d3f548f18df84a41bca
Ryan Lei <ryanlei@xm.wonder.com>
2025-08-06 11:00:25 +0800

    availableProcessors

--- stu/src/main/java/com/lei/jvm/stu/system/JVMSysTest.java
+++ stu/src/main/java/com/lei/jvm/stu/system/JVMSysTest.java
@@ -1,6 +1,7 @@
 package com.lei.jvm.stu.system;
 
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Properties;
 
@@ -10,13 +11,18 @@ import java.util.Properties;
  * @Version 1.0
  * @Description
  */
+@Slf4j
 public class JVMSysTest {
 
     public static void main(String[] args) {
+        log.info("核心数={}", Runtime.getRuntime().availableProcessors());
+    }
+
+    private static void property() {
         System.out.println(Integer.MAX_VALUE);
 
         Properties properties = System.getProperties();
-        System.out.println(JSON.toJSONString(properties,true));
+        System.out.println(JSON.toJSONString(properties, true));
     }
 
 }
