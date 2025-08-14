d99b31f058a48784ec8435d26d8fbd3711141648
Ryan Lei <ryanlei@xm.wonder.com>
2025-08-14 13:58:43 +0800

    IntStreamTest

---
+++ stu/src/main/java/com/lei/jvm/stu/base/IntStreamTest.java
@@ -0,0 +1,21 @@
+package com.lei.jvm.stu.base;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+/**
+ * @author ryan
+ */
+@Slf4j
+public class IntStreamTest {
+
+    public static void main(String[] args) {
+        List<String> list = IntStream.range(0, 2).mapToObj(x -> UUID.randomUUID().toString()).toList();
+        log.info("结果={}", JSON.toJSONString(list));
+    }
+
+}
